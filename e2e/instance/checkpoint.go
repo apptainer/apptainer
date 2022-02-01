b75ae474e13bb1d19075789b04356d1c25827ed0
Dave Dykstra <2129743+DrDaveD@users.noreply.github.com>
2022-02-01 13:27:06 -0600

    replace year in Apptainer copyright with "Contributors"
    Signed-off-by: Dave Dykstra <2129743+DrDaveD@users.noreply.github.com>

--- e2e/instance/checkpoint.go
+++ e2e/instance/checkpoint.go
@@ -1,4 +1,5 @@
-// Copyright (c) 2021 Apptainer a Series of LF Projects LLC
+// Copyright (c) Contributors to the Apptainer project, established as
+//   Apptainer a Series of LF Projects LLC.
 //   For website terms of use, trademark policy, privacy policy and other
 //   project policies see https://lfprojects.org/policies
 // This software is licensed under a 3-clause BSD license. Please consult the
