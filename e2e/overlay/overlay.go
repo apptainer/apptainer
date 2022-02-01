f83c99a92151a017338aae5523770057c3da4441
Dave Dykstra <2129743+DrDaveD@users.noreply.github.com>
2022-02-01 13:27:06 -0600

    replace year in Apptainer copyright with "Contributors"
    Signed-off-by: Dave Dykstra <2129743+DrDaveD@users.noreply.github.com>

--- e2e/overlay/overlay.go
+++ e2e/overlay/overlay.go
@@ -1,4 +1,5 @@
-// Copyright (c) 2021-2022 Apptainer a Series of LF Projects LLC
+// Copyright (c) Contributors to the Apptainer project, established as
+//   Apptainer a Series of LF Projects LLC.
 //   For website terms of use, trademark policy, privacy policy and other
 //   project policies see https://lfprojects.org/policies
 
