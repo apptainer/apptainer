sylabs/singularity

refactor: improve unit tests for cgroups v1 & v2 unified

internal/pkg/cgroups/manager_linux_test.go
@@ -9,13 +9,38 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"os/exec"
+	"path/filepath"
 	"strconv"
 	"strings"
 	"testing"
+
+	"github.com/sylabs/singularity/internal/pkg/test"
+	"github.com/sylabs/singularity/internal/pkg/test/tool/require"
 )
 
-// ensureIntInFile asserts that the content of path is the integer wantInt
-func ensureIntInFile(t *testing.T, path string, wantInt int64) {
+// This file contains tests that will run under cgroups v1 & v2, and test utility functions.
+
+func TestGetFromPid(t *testing.T) {
+	test.EnsurePrivilege(t)
+	require.Cgroups(t)
+
+	pid, manager, cleanup := testManager(t)
+	defer cleanup()
+
+	// Covers GetManagerForPath indirectly
+	pidMgr, err := GetManagerForPid(pid)
+	if err != nil {
+		t.Fatalf("While getting cgroup manager for pid: %v", err)
+	}
+
+	if pidMgr.group != manager.group {
+		t.Errorf("Expected %s for cgroup from pid, got %s", manager.group, pidMgr.cgroup)
+	}
+}
+
+// ensureInt asserts that the content of path is the integer wantInt
+func ensureInt(t *testing.T, path string, wantInt int64) {
 	file, err := os.Open(path)
 	if err != nil {
 		t.Errorf("while opening %q: %v", path, err)
@@ -37,6 +62,30 @@ func ensureIntInFile(t *testing.T, path string, wantInt int64) {
 	if val != wantInt {
 		t.Errorf("found %d in %q, expected %d", val, path, wantInt)
 	}
+}
+
+// ensureContainsInt asserts that the content of path contains the integer wantInt
+func ensureContainsInt(t *testing.T, path string, wantInt int64) {
+	file, err := os.Open(path)
+	if err != nil {
+		t.Errorf("while opening %q: %v", path, err)
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		val, err := strconv.ParseInt(scanner.Text(), 10, 64)
+		if err != nil {
+			t.Errorf("could not parse %q: %v", path, err)
+		}
+		if val == wantInt {
+			return
+		}
+	}
+
+	t.Fatalf("%s did not contain expected value %d", path, wantInt)
 }
 
 // ensureState asserts that a process pid has the required state
@@ -64,4 +113,40 @@ func ensureState(t *testing.T, pid int, wantStates string) {
 	if !strings.ContainsAny(procState, wantStates) {
 		t.Errorf("Process %d had state %q, expected state %q", pid, procState, wantStates)
 	}
+}
+
+// testManager returns a cgroup manager, that has created a cgroup with a `cat /dev/zero` process,
+// and example resource config.
+func testManager(t *testing.T) (pid int, manager *Manager, cleanup func()) {
+	// Create process to put into a cgroup
+	t.Log("Creating test process")
+	cmd := exec.Command("/bin/cat", "/dev/zero")
+	if err := cmd.Start(); err != nil {
+		t.Fatalf("While starting test process: %v", err)
+	}
+	pid = cmd.Process.Pid
+	strPid := strconv.Itoa(pid)
+	group := filepath.Join("/singularity", strPid)
+
+	cgroupsToml := "example/cgroups.toml"
+	// Some systems, e.g. ppc64le may not have a 2MB page size, so don't
+	// apply a 2MB hugetlb limit if that's the case.
+	_, err := os.Stat("/sys/fs/cgroup/dev-hugepages.mount/hugetlb.2MB.max")
+	if os.IsNotExist(err) {
+		t.Log("No hugetlb.2MB.max - using alternate cgroups test file")
+		cgroupsToml = "example/cgroups-no-hugetlb.toml"
+	}
+
+	manager, err = NewManagerWithFile(cgroupsToml, pid, group)
+	if err != nil {
+		t.Fatalf("While creating new cgroup: %v", err)
+	}
+
+	cleanup = func() {
+		cmd.Process.Kill()
+		cmd.Process.Wait()
+		manager.Destroy()
+	}
+
+	return pid, manager, cleanup
 }

internal/pkg/cgroups/manager_linux_v1_test.go
@@ -10,88 +10,107 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
-	"strconv"
 	"testing"
 
+	specs "github.com/opencontainers/runtime-spec/specs-go"
 	"github.com/sylabs/singularity/internal/pkg/test"
 	"github.com/sylabs/singularity/internal/pkg/test/tool/require"
 )
 
 // This file contains tests that will run under cgroups v1 only.
 
-//nolint:dupl
 func TestCgroupsV1(t *testing.T) {
 	test.EnsurePrivilege(t)
 	require.CgroupsV1(t)
+	t.Run("GetCgroupRootPath", testGetCgroupRootPathV1)
+	t.Run("NewUpdate", testNewUpdateV1)
+	t.Run("AddProc", testAddProcV1)
+	t.Run("FreezeThaw", testFreezeThawV1)
+}
 
-	// Create process to put into a cgroup
+//nolint:dupl
+func testGetCgroupRootPathV1(t *testing.T) {
+	// This cgroup won't be created in the fs as we don't add a PID through the manager
+	group := filepath.Join("/singularity", "rootpathtest")
+	manager, err := newManager(&specs.LinuxResources{}, group)
+	if err != nil {
+		t.Fatalf("While creating manager: %v", err)
+	}
+
+	rootPath, err := manager.GetCgroupRootPath()
+	if err != nil {
+		t.Errorf("While getting root path: %v", err)
+	}
+	// Cgroups v2 has a fixed mount point
+	if rootPath != unifiedMountPoint {
+		t.Errorf("Expected %s, got %s", unifiedMountPoint, rootPath)
+	}
+}
+
+//nolint:dupl
+func testNewUpdateV1(t *testing.T) {
+	_, manager, cleanup := testManager(t)
+	defer cleanup()
+
+	// Check for correct 1024 value
+	rootPath, err := manager.GetCgroupRootPath()
+	if err != nil {
+		t.Fatalf("can't determine cgroups root path, is cgroups enabled ?")
+	}
+	pidsMax := filepath.Join(rootPath, "pids", manager.group, "pids.limit")
+	ensureInt(t, pidsMax, 1024)
+
+	// Write a new config with [pids] limit = 512
+	content := []byte("[pids]\nlimit = 512")
+	tmpfile, err := ioutil.TempFile("", "cgroups")
+	if err != nil {
+		t.Fatalf("While creating update file: %v", err)
+	}
+	defer os.Remove(tmpfile.Name())
+	if _, err := tmpfile.Write(content); err != nil {
+		t.Fatalf("While writing update file: %v", err)
+	}
+	if err := tmpfile.Close(); err != nil {
+		t.Fatalf("While closing update file: %v", err)
+	}
+
+	// Update existing cgroup from new config
+	if err := manager.UpdateFromFile(tmpfile.Name()); err != nil {
+		t.Fatalf("While updating cgroup: %v", err)
+	}
+
+	// Check pids.max is now 512
+	ensureInt(t, pidsMax, 512)
+}
+
+func testAddProcV1(t *testing.T) {
+	pid, manager, cleanup := testManager(t)
+
 	cmd := exec.Command("/bin/cat", "/dev/zero")
 	if err := cmd.Start(); err != nil {
-		t.Fatal(err)
+		t.Fatalf("While starting test process: %v", err)
 	}
-
-	pid := cmd.Process.Pid
-	strPid := strconv.Itoa(pid)
-	group := filepath.Join("/singularity", strPid)
-
-	cgroupsToml := "example/cgroups.toml"
-	// Some systems, e.g. ppc64le may not have a 2MB page size, so don't
-	// apply a 2MB hugetlb limit if that's the case.
-	_, err := os.Stat("/sys/fs/cgroup/hugetlb/hugetlb.2MB.limit_in_bytes")
-	if os.IsNotExist(err) {
-		t.Log("No hugetlb.2MB.limit_in_bytes - using alternate cgroups test file")
-		cgroupsToml = "example/cgroups-no-hugetlb.toml"
-	}
-
-	manager, err := NewManagerWithFile(cgroupsToml, pid, group)
-	if err != nil {
-		t.Fatal(err)
-	}
-
+	newPid := cmd.Process.Pid
 	defer func() {
 		cmd.Process.Kill()
 		cmd.Process.Wait()
-		manager.Destroy()
+		cleanup()
 	}()
+
+	if err := manager.AddProc(newPid); err != nil {
+		t.Errorf("While adding proc to cgroup: %v", err)
+	}
 
 	rootPath, err := manager.GetCgroupRootPath()
 	if err != nil {
 		t.Fatalf("can't determine cgroups root path, is cgroups enabled ?")
 	}
-
-	cpuShares := filepath.Join(rootPath, "cpu", group, "cpu.shares")
-	ensureIntInFile(t, cpuShares, 1024)
-
-	content := []byte("[cpu]\nshares = 512")
-	tmpfile, err := ioutil.TempFile("", "cgroups")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer os.Remove(tmpfile.Name())
-	if _, err := tmpfile.Write(content); err != nil {
-		t.Fatal(err)
-	}
-	if err := tmpfile.Close(); err != nil {
-		t.Fatal(err)
-	}
-
-	manager, err = GetManagerForPid(pid)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Update existing cgroup from new config
-	if err := manager.UpdateFromFile(tmpfile.Name()); err != nil {
-		t.Fatal(err)
-	}
-	ensureIntInFile(t, cpuShares, 512)
+	cgroupProcs := filepath.Join(rootPath, "pids", manager.group, "cgroup.procs")
+	ensureContainsInt(t, cgroupProcs, int64(pid))
+	ensureContainsInt(t, cgroupProcs, int64(newPid))
 }
 
-//nolint:dupl
-func TestPauseResumeV1(t *testing.T) {
-	test.EnsurePrivilege(t)
-	require.CgroupsV1(t)
-
+func testFreezeThawV1(t *testing.T) {
 	manager := &Manager{}
 	if err := manager.Freeze(); err == nil {
 		t.Errorf("unexpected success with PID 0")
@@ -100,27 +119,13 @@ func TestPauseResumeV1(t *testing.T) {
 		t.Errorf("unexpected success with PID 0")
 	}
 
-	cmd := exec.Command("/bin/cat", "/dev/zero")
-	if err := cmd.Start(); err != nil {
-		t.Fatal(err)
-	}
-
-	group := filepath.Join("/singularity", strconv.Itoa(cmd.Process.Pid))
-	manager, err := NewManagerWithFile("example/cgroups.toml", cmd.Process.Pid, group)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	defer func() {
-		cmd.Process.Kill()
-		cmd.Process.Wait()
-		manager.Destroy()
-	}()
+	pid, manager, cleanup := testManager(t)
+	defer cleanup()
 
 	manager.Freeze()
 	// cgroups v1 freeze is to uninterruptible sleep
-	ensureState(t, cmd.Process.Pid, "D")
+	ensureState(t, pid, "D")
 
 	manager.Thaw()
-	ensureState(t, cmd.Process.Pid, "RS")
+	ensureState(t, pid, "RS")
 }

internal/pkg/cgroups/manager_linux_v2_test.go
@@ -11,122 +11,133 @@ import (
 	"os/exec"
 	"path"
 	"path/filepath"
-	"strconv"
 	"testing"
 
+	"github.com/opencontainers/runtime-spec/specs-go"
 	"github.com/sylabs/singularity/internal/pkg/test"
 	"github.com/sylabs/singularity/internal/pkg/test/tool/require"
 )
 
 // This file contains tests that will run under cgroups v2 only.
 
-//nolint:dupl
 func TestCgroupsV2(t *testing.T) {
 	test.EnsurePrivilege(t)
 	require.CgroupsV2Unified(t)
+	t.Run("GetCgroupRootPath", testGetCgroupRootPathV2)
+	t.Run("NewUpdate", testNewUpdateV2)
+	t.Run("AddProc", testAddProcV2)
+	t.Run("FreezeThaw", testFreezeThawV2)
+}
 
-	// Create process to put into a cgroup
-	cmd := exec.Command("/bin/cat", "/dev/zero")
-	if err := cmd.Start(); err != nil {
-		t.Fatal(err)
-	}
+//nolint:dupl
+func testGetCgroupRootPathV2(t *testing.T) {
+	test.EnsurePrivilege(t)
+	require.CgroupsV2Unified(t)
 
-	pid := cmd.Process.Pid
-	strPid := strconv.Itoa(pid)
-	group := filepath.Join("/singularity", strPid)
-
-	cgroupsToml := "example/cgroups.toml"
-	// Some systems, e.g. ppc64le may not have a 2MB page size, so don't
-	// apply a 2MB hugetlb limit if that's the case.
-	_, err := os.Stat("/sys/fs/cgroup/dev-hugepages.mount/hugetlb.2MB.max")
-	if os.IsNotExist(err) {
-		t.Log("No hugetlb.2MB.max - using alternate cgroups test file")
-		cgroupsToml = "example/cgroups-no-hugetlb.toml"
-	}
-
-	manager, err := NewManagerWithFile(cgroupsToml, pid, group)
+	// This cgroup won't be created in the fs as we don't add a PID through the manager
+	group := filepath.Join("/singularity", "rootpathtest")
+	manager, err := newManager(&specs.LinuxResources{}, group)
 	if err != nil {
-		t.Fatal(err)
+		t.Fatalf("While creating manager: %v", err)
 	}
 
-	defer func() {
-		cmd.Process.Kill()
-		cmd.Process.Wait()
-		manager.Destroy()
-	}()
+	rootPath, err := manager.GetCgroupRootPath()
+	if err != nil {
+		t.Errorf("While getting root path: %v", err)
+	}
+	// Cgroups v2 has a fixed mount point
+	if rootPath != unifiedMountPoint {
+		t.Errorf("Expected %s, got %s", unifiedMountPoint, rootPath)
+	}
+}
+
+//nolint:dupl
+func testNewUpdateV2(t *testing.T) {
+	test.EnsurePrivilege(t)
+	require.CgroupsV2Unified(t)
+
+	_, manager, cleanup := testManager(t)
+	defer cleanup()
 
 	// For cgroups v2 [pids] limit -> pids.max
 	// Check for correct 1024 value
-	pidsMax := filepath.Join("/sys/fs/cgroup", group, "pids.max")
-	ensureIntInFile(t, pidsMax, 1024)
+	pidsMax := filepath.Join("/sys/fs/cgroup", manager.group, "pids.max")
+	ensureInt(t, pidsMax, 1024)
 
 	// Write a new config with [pids] limit = 512
 	content := []byte("[pids]\nlimit = 512")
 	tmpfile, err := ioutil.TempFile("", "cgroups")
 	if err != nil {
-		t.Fatal(err)
+		t.Fatalf("While creating update file: %v", err)
 	}
 	defer os.Remove(tmpfile.Name())
 	if _, err := tmpfile.Write(content); err != nil {
-		t.Fatal(err)
+		t.Fatalf("While writing update file: %v", err)
 	}
 	if err := tmpfile.Close(); err != nil {
-		t.Fatal(err)
-	}
-
-	manager, err = GetManagerForPid(pid)
-	if err != nil {
-		t.Fatal(err)
+		t.Fatalf("While closing update file: %v", err)
 	}
 
 	// Update existing cgroup from new config
 	if err := manager.UpdateFromFile(tmpfile.Name()); err != nil {
-		t.Fatal(err)
+		t.Fatalf("While updating cgroup: %v", err)
 	}
 
 	// Check pids.max is now 512
-	ensureIntInFile(t, pidsMax, 512)
+	ensureInt(t, pidsMax, 512)
+}
+
+func testAddProcV2(t *testing.T) {
+	test.EnsurePrivilege(t)
+	require.CgroupsV2Unified(t)
+
+	pid, manager, cleanup := testManager(t)
+
+	cmd := exec.Command("/bin/cat", "/dev/zero")
+	if err := cmd.Start(); err != nil {
+		t.Fatalf("While starting test process: %v", err)
+	}
+	newPid := cmd.Process.Pid
+	defer func() {
+		cmd.Process.Kill()
+		cmd.Process.Wait()
+		cleanup()
+	}()
+
+	if err := manager.AddProc(newPid); err != nil {
+		t.Errorf("While adding proc to cgroup: %v", err)
+	}
+
+	cgroupProcs := filepath.Join("/sys/fs/cgroup", manager.group, "cgroup.procs")
+	ensureContainsInt(t, cgroupProcs, int64(pid))
+	ensureContainsInt(t, cgroupProcs, int64(newPid))
 }
 
 //nolint:dupl
-func TestPauseResumeV2(t *testing.T) {
+func testFreezeThawV2(t *testing.T) {
 	test.EnsurePrivilege(t)
 	require.CgroupsV2Unified(t)
 
 	manager := &Manager{}
 	if err := manager.Freeze(); err == nil {
-		t.Errorf("unexpected success with PID 0")
+		t.Errorf("unexpected success freezing PID 0")
 	}
 	if err := manager.Thaw(); err == nil {
-		t.Errorf("unexpected success with PID 0")
+		t.Errorf("unexpected success thawing PID 0")
 	}
 
-	cmd := exec.Command("/bin/cat", "/dev/zero")
-	if err := cmd.Start(); err != nil {
-		t.Fatal(err)
-	}
-
-	group := filepath.Join("/singularity", strconv.Itoa(cmd.Process.Pid))
-	manager, err := NewManagerWithFile("example/cgroups.toml", cmd.Process.Pid, group)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	defer func() {
-		cmd.Process.Kill()
-		cmd.Process.Wait()
-		manager.Destroy()
-	}()
+	pid, manager, cleanup := testManager(t)
+	defer cleanup()
 
 	manager.Freeze()
 	// cgroups v2 freeze is to interruptible sleep, which could actually occur
 	// for our cat /dev/zero while it's running, so check freeze marker as well
 	// as the process state here.
-	ensureState(t, cmd.Process.Pid, "S")
+	ensureState(t, pid, "S")
 	freezePath := path.Join("/sys/fs/cgroup", manager.group, "cgroup.freeze")
-	ensureIntInFile(t, freezePath, 1)
+	ensureInt(t, freezePath, 1)
 
 	manager.Thaw()
-	ensureState(t, cmd.Process.Pid, "RS")
-	ensureIntInFile(t, freezePath, 0)
+	ensureState(t, pid, "RS")
+	ensureInt(t, freezePath, 0)
 }

apptainer/apptainer

Signed-off-by: Edita Kizinevic <edita.kizinevic@cern.ch>

internal/pkg/cgroups/manager_linux_test.go
@@ -13,13 +13,38 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"os/exec"
+	"path/filepath"
 	"strconv"
 	"strings"
 	"testing"
+
+	"github.com/apptainer/apptainer/internal/pkg/test"
+	"github.com/apptainer/apptainer/internal/pkg/test/tool/require"
 )
 
-// ensureIntInFile asserts that the content of path is the integer wantInt
-func ensureIntInFile(t *testing.T, path string, wantInt int64) {
+// This file contains tests that will run under cgroups v1 & v2, and test utility functions.
+
+func TestGetFromPid(t *testing.T) {
+	test.EnsurePrivilege(t)
+	require.Cgroups(t)
+
+	pid, manager, cleanup := testManager(t)
+	defer cleanup()
+
+	// Covers GetManagerForPath indirectly
+	pidMgr, err := GetManagerForPid(pid)
+	if err != nil {
+		t.Fatalf("While getting cgroup manager for pid: %v", err)
+	}
+
+	if pidMgr.group != manager.group {
+		t.Errorf("Expected %s for cgroup from pid, got %s", manager.group, pidMgr.cgroup)
+	}
+}
+
+// ensureInt asserts that the content of path is the integer wantInt
+func ensureInt(t *testing.T, path string, wantInt int64) {
 	file, err := os.Open(path)
 	if err != nil {
 		t.Errorf("while opening %q: %v", path, err)
@@ -41,6 +66,30 @@ func ensureIntInFile(t *testing.T, path string, wantInt int64) {
 	if val != wantInt {
 		t.Errorf("found %d in %q, expected %d", val, path, wantInt)
 	}
+}
+
+// ensureContainsInt asserts that the content of path contains the integer wantInt
+func ensureContainsInt(t *testing.T, path string, wantInt int64) {
+	file, err := os.Open(path)
+	if err != nil {
+		t.Errorf("while opening %q: %v", path, err)
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		val, err := strconv.ParseInt(scanner.Text(), 10, 64)
+		if err != nil {
+			t.Errorf("could not parse %q: %v", path, err)
+		}
+		if val == wantInt {
+			return
+		}
+	}
+
+	t.Fatalf("%s did not contain expected value %d", path, wantInt)
 }
 
 // ensureState asserts that a process pid has the required state
@@ -68,4 +117,40 @@ func ensureState(t *testing.T, pid int, wantStates string) {
 	if !strings.ContainsAny(procState, wantStates) {
 		t.Errorf("Process %d had state %q, expected state %q", pid, procState, wantStates)
 	}
+}
+
+// testManager returns a cgroup manager, that has created a cgroup with a `cat /dev/zero` process,
+// and example resource config.
+func testManager(t *testing.T) (pid int, manager *Manager, cleanup func()) {
+	// Create process to put into a cgroup
+	t.Log("Creating test process")
+	cmd := exec.Command("/bin/cat", "/dev/zero")
+	if err := cmd.Start(); err != nil {
+		t.Fatalf("While starting test process: %v", err)
+	}
+	pid = cmd.Process.Pid
+	strPid := strconv.Itoa(pid)
+	group := filepath.Join("/apptainer", strPid)
+
+	cgroupsToml := "example/cgroups.toml"
+	// Some systems, e.g. ppc64le may not have a 2MB page size, so don't
+	// apply a 2MB hugetlb limit if that's the case.
+	_, err := os.Stat("/sys/fs/cgroup/dev-hugepages.mount/hugetlb.2MB.max")
+	if os.IsNotExist(err) {
+		t.Log("No hugetlb.2MB.max - using alternate cgroups test file")
+		cgroupsToml = "example/cgroups-no-hugetlb.toml"
+	}
+
+	manager, err = NewManagerWithFile(cgroupsToml, pid, group)
+	if err != nil {
+		t.Fatalf("While creating new cgroup: %v", err)
+	}
+
+	cleanup = func() {
+		cmd.Process.Kill()
+		cmd.Process.Wait()
+		manager.Destroy()
+	}
+
+	return pid, manager, cleanup
 }

internal/pkg/cgroups/manager_linux_v1_test.go
@@ -14,88 +14,107 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
-	"strconv"
 	"testing"
 
 	"github.com/apptainer/apptainer/internal/pkg/test"
 	"github.com/apptainer/apptainer/internal/pkg/test/tool/require"
+	specs "github.com/opencontainers/runtime-spec/specs-go"
 )
 
 // This file contains tests that will run under cgroups v1 only.
 
-//nolint:dupl
 func TestCgroupsV1(t *testing.T) {
 	test.EnsurePrivilege(t)
 	require.CgroupsV1(t)
+	t.Run("GetCgroupRootPath", testGetCgroupRootPathV1)
+	t.Run("NewUpdate", testNewUpdateV1)
+	t.Run("AddProc", testAddProcV1)
+	t.Run("FreezeThaw", testFreezeThawV1)
+}
 
-	// Create process to put into a cgroup
+//nolint:dupl
+func testGetCgroupRootPathV1(t *testing.T) {
+	// This cgroup won't be created in the fs as we don't add a PID through the manager
+	group := filepath.Join("/apptainer", "rootpathtest")
+	manager, err := newManager(&specs.LinuxResources{}, group)
+	if err != nil {
+		t.Fatalf("While creating manager: %v", err)
+	}
+
+	rootPath, err := manager.GetCgroupRootPath()
+	if err != nil {
+		t.Errorf("While getting root path: %v", err)
+	}
+	// Cgroups v2 has a fixed mount point
+	if rootPath != unifiedMountPoint {
+		t.Errorf("Expected %s, got %s", unifiedMountPoint, rootPath)
+	}
+}
+
+//nolint:dupl
+func testNewUpdateV1(t *testing.T) {
+	_, manager, cleanup := testManager(t)
+	defer cleanup()
+
+	// Check for correct 1024 value
+	rootPath, err := manager.GetCgroupRootPath()
+	if err != nil {
+		t.Fatalf("can't determine cgroups root path, is cgroups enabled ?")
+	}
+	pidsMax := filepath.Join(rootPath, "pids", manager.group, "pids.limit")
+	ensureInt(t, pidsMax, 1024)
+
+	// Write a new config with [pids] limit = 512
+	content := []byte("[pids]\nlimit = 512")
+	tmpfile, err := ioutil.TempFile("", "cgroups")
+	if err != nil {
+		t.Fatalf("While creating update file: %v", err)
+	}
+	defer os.Remove(tmpfile.Name())
+	if _, err := tmpfile.Write(content); err != nil {
+		t.Fatalf("While writing update file: %v", err)
+	}
+	if err := tmpfile.Close(); err != nil {
+		t.Fatalf("While closing update file: %v", err)
+	}
+
+	// Update existing cgroup from new config
+	if err := manager.UpdateFromFile(tmpfile.Name()); err != nil {
+		t.Fatalf("While updating cgroup: %v", err)
+	}
+
+	// Check pids.max is now 512
+	ensureInt(t, pidsMax, 512)
+}
+
+func testAddProcV1(t *testing.T) {
+	pid, manager, cleanup := testManager(t)
+
 	cmd := exec.Command("/bin/cat", "/dev/zero")
 	if err := cmd.Start(); err != nil {
-		t.Fatal(err)
+		t.Fatalf("While starting test process: %v", err)
 	}
-
-	pid := cmd.Process.Pid
-	strPid := strconv.Itoa(pid)
-	group := filepath.Join("/apptainer", strPid)
-
-	cgroupsToml := "example/cgroups.toml"
-	// Some systems, e.g. ppc64le may not have a 2MB page size, so don't
-	// apply a 2MB hugetlb limit if that's the case.
-	_, err := os.Stat("/sys/fs/cgroup/hugetlb/hugetlb.2MB.limit_in_bytes")
-	if os.IsNotExist(err) {
-		t.Log("No hugetlb.2MB.limit_in_bytes - using alternate cgroups test file")
-		cgroupsToml = "example/cgroups-no-hugetlb.toml"
-	}
-
-	manager, err := NewManagerWithFile(cgroupsToml, pid, group)
-	if err != nil {
-		t.Fatal(err)
-	}
-
+	newPid := cmd.Process.Pid
 	defer func() {
 		cmd.Process.Kill()
 		cmd.Process.Wait()
-		manager.Destroy()
+		cleanup()
 	}()
+
+	if err := manager.AddProc(newPid); err != nil {
+		t.Errorf("While adding proc to cgroup: %v", err)
+	}
 
 	rootPath, err := manager.GetCgroupRootPath()
 	if err != nil {
 		t.Fatalf("can't determine cgroups root path, is cgroups enabled ?")
 	}
-
-	cpuShares := filepath.Join(rootPath, "cpu", group, "cpu.shares")
-	ensureIntInFile(t, cpuShares, 1024)
-
-	content := []byte("[cpu]\nshares = 512")
-	tmpfile, err := ioutil.TempFile("", "cgroups")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer os.Remove(tmpfile.Name())
-	if _, err := tmpfile.Write(content); err != nil {
-		t.Fatal(err)
-	}
-	if err := tmpfile.Close(); err != nil {
-		t.Fatal(err)
-	}
-
-	manager, err = GetManagerForPid(pid)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Update existing cgroup from new config
-	if err := manager.UpdateFromFile(tmpfile.Name()); err != nil {
-		t.Fatal(err)
-	}
-	ensureIntInFile(t, cpuShares, 512)
+	cgroupProcs := filepath.Join(rootPath, "pids", manager.group, "cgroup.procs")
+	ensureContainsInt(t, cgroupProcs, int64(pid))
+	ensureContainsInt(t, cgroupProcs, int64(newPid))
 }
 
-//nolint:dupl
-func TestPauseResumeV1(t *testing.T) {
-	test.EnsurePrivilege(t)
-	require.CgroupsV1(t)
-
+func testFreezeThawV1(t *testing.T) {
 	manager := &Manager{}
 	if err := manager.Freeze(); err == nil {
 		t.Errorf("unexpected success with PID 0")
@@ -104,27 +123,13 @@ func TestPauseResumeV1(t *testing.T) {
 		t.Errorf("unexpected success with PID 0")
 	}
 
-	cmd := exec.Command("/bin/cat", "/dev/zero")
-	if err := cmd.Start(); err != nil {
-		t.Fatal(err)
-	}
-
-	group := filepath.Join("/apptainer", strconv.Itoa(cmd.Process.Pid))
-	manager, err := NewManagerWithFile("example/cgroups.toml", cmd.Process.Pid, group)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	defer func() {
-		cmd.Process.Kill()
-		cmd.Process.Wait()
-		manager.Destroy()
-	}()
+	pid, manager, cleanup := testManager(t)
+	defer cleanup()
 
 	manager.Freeze()
 	// cgroups v1 freeze is to uninterruptible sleep
-	ensureState(t, cmd.Process.Pid, "D")
+	ensureState(t, pid, "D")
 
 	manager.Thaw()
-	ensureState(t, cmd.Process.Pid, "RS")
+	ensureState(t, pid, "RS")
 }

internal/pkg/cgroups/manager_linux_v2_test.go
@@ -15,122 +15,133 @@ import (
 	"os/exec"
 	"path"
 	"path/filepath"
-	"strconv"
 	"testing"
 
 	"github.com/apptainer/apptainer/internal/pkg/test"
 	"github.com/apptainer/apptainer/internal/pkg/test/tool/require"
+	"github.com/opencontainers/runtime-spec/specs-go"
 )
 
 // This file contains tests that will run under cgroups v2 only.
 
-//nolint:dupl
 func TestCgroupsV2(t *testing.T) {
 	test.EnsurePrivilege(t)
 	require.CgroupsV2Unified(t)
+	t.Run("GetCgroupRootPath", testGetCgroupRootPathV2)
+	t.Run("NewUpdate", testNewUpdateV2)
+	t.Run("AddProc", testAddProcV2)
+	t.Run("FreezeThaw", testFreezeThawV2)
+}
 
-	// Create process to put into a cgroup
-	cmd := exec.Command("/bin/cat", "/dev/zero")
-	if err := cmd.Start(); err != nil {
-		t.Fatal(err)
-	}
+//nolint:dupl
+func testGetCgroupRootPathV2(t *testing.T) {
+	test.EnsurePrivilege(t)
+	require.CgroupsV2Unified(t)
 
-	pid := cmd.Process.Pid
-	strPid := strconv.Itoa(pid)
-	group := filepath.Join("/apptainer", strPid)
-
-	cgroupsToml := "example/cgroups.toml"
-	// Some systems, e.g. ppc64le may not have a 2MB page size, so don't
-	// apply a 2MB hugetlb limit if that's the case.
-	_, err := os.Stat("/sys/fs/cgroup/dev-hugepages.mount/hugetlb.2MB.max")
-	if os.IsNotExist(err) {
-		t.Log("No hugetlb.2MB.max - using alternate cgroups test file")
-		cgroupsToml = "example/cgroups-no-hugetlb.toml"
-	}
-
-	manager, err := NewManagerWithFile(cgroupsToml, pid, group)
+	// This cgroup won't be created in the fs as we don't add a PID through the manager
+	group := filepath.Join("/apptainer", "rootpathtest")
+	manager, err := newManager(&specs.LinuxResources{}, group)
 	if err != nil {
-		t.Fatal(err)
+		t.Fatalf("While creating manager: %v", err)
 	}
 
-	defer func() {
-		cmd.Process.Kill()
-		cmd.Process.Wait()
-		manager.Destroy()
-	}()
+	rootPath, err := manager.GetCgroupRootPath()
+	if err != nil {
+		t.Errorf("While getting root path: %v", err)
+	}
+	// Cgroups v2 has a fixed mount point
+	if rootPath != unifiedMountPoint {
+		t.Errorf("Expected %s, got %s", unifiedMountPoint, rootPath)
+	}
+}
+
+//nolint:dupl
+func testNewUpdateV2(t *testing.T) {
+	test.EnsurePrivilege(t)
+	require.CgroupsV2Unified(t)
+
+	_, manager, cleanup := testManager(t)
+	defer cleanup()
 
 	// For cgroups v2 [pids] limit -> pids.max
 	// Check for correct 1024 value
-	pidsMax := filepath.Join("/sys/fs/cgroup", group, "pids.max")
-	ensureIntInFile(t, pidsMax, 1024)
+	pidsMax := filepath.Join("/sys/fs/cgroup", manager.group, "pids.max")
+	ensureInt(t, pidsMax, 1024)
 
 	// Write a new config with [pids] limit = 512
 	content := []byte("[pids]\nlimit = 512")
 	tmpfile, err := ioutil.TempFile("", "cgroups")
 	if err != nil {
-		t.Fatal(err)
+		t.Fatalf("While creating update file: %v", err)
 	}
 	defer os.Remove(tmpfile.Name())
 	if _, err := tmpfile.Write(content); err != nil {
-		t.Fatal(err)
+		t.Fatalf("While writing update file: %v", err)
 	}
 	if err := tmpfile.Close(); err != nil {
-		t.Fatal(err)
-	}
-
-	manager, err = GetManagerForPid(pid)
-	if err != nil {
-		t.Fatal(err)
+		t.Fatalf("While closing update file: %v", err)
 	}
 
 	// Update existing cgroup from new config
 	if err := manager.UpdateFromFile(tmpfile.Name()); err != nil {
-		t.Fatal(err)
+		t.Fatalf("While updating cgroup: %v", err)
 	}
 
 	// Check pids.max is now 512
-	ensureIntInFile(t, pidsMax, 512)
+	ensureInt(t, pidsMax, 512)
+}
+
+func testAddProcV2(t *testing.T) {
+	test.EnsurePrivilege(t)
+	require.CgroupsV2Unified(t)
+
+	pid, manager, cleanup := testManager(t)
+
+	cmd := exec.Command("/bin/cat", "/dev/zero")
+	if err := cmd.Start(); err != nil {
+		t.Fatalf("While starting test process: %v", err)
+	}
+	newPid := cmd.Process.Pid
+	defer func() {
+		cmd.Process.Kill()
+		cmd.Process.Wait()
+		cleanup()
+	}()
+
+	if err := manager.AddProc(newPid); err != nil {
+		t.Errorf("While adding proc to cgroup: %v", err)
+	}
+
+	cgroupProcs := filepath.Join("/sys/fs/cgroup", manager.group, "cgroup.procs")
+	ensureContainsInt(t, cgroupProcs, int64(pid))
+	ensureContainsInt(t, cgroupProcs, int64(newPid))
 }
 
 //nolint:dupl
-func TestPauseResumeV2(t *testing.T) {
+func testFreezeThawV2(t *testing.T) {
 	test.EnsurePrivilege(t)
 	require.CgroupsV2Unified(t)
 
 	manager := &Manager{}
 	if err := manager.Freeze(); err == nil {
-		t.Errorf("unexpected success with PID 0")
+		t.Errorf("unexpected success freezing PID 0")
 	}
 	if err := manager.Thaw(); err == nil {
-		t.Errorf("unexpected success with PID 0")
+		t.Errorf("unexpected success thawing PID 0")
 	}
 
-	cmd := exec.Command("/bin/cat", "/dev/zero")
-	if err := cmd.Start(); err != nil {
-		t.Fatal(err)
-	}
-
-	group := filepath.Join("/apptainer", strconv.Itoa(cmd.Process.Pid))
-	manager, err := NewManagerWithFile("example/cgroups.toml", cmd.Process.Pid, group)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	defer func() {
-		cmd.Process.Kill()
-		cmd.Process.Wait()
-		manager.Destroy()
-	}()
+	pid, manager, cleanup := testManager(t)
+	defer cleanup()
 
 	manager.Freeze()
 	// cgroups v2 freeze is to interruptible sleep, which could actually occur
 	// for our cat /dev/zero while it's running, so check freeze marker as well
 	// as the process state here.
-	ensureState(t, cmd.Process.Pid, "S")
+	ensureState(t, pid, "S")
 	freezePath := path.Join("/sys/fs/cgroup", manager.group, "cgroup.freeze")
-	ensureIntInFile(t, freezePath, 1)
+	ensureInt(t, freezePath, 1)
 
 	manager.Thaw()
-	ensureState(t, cmd.Process.Pid, "RS")
-	ensureIntInFile(t, freezePath, 0)
+	ensureState(t, pid, "RS")
+	ensureInt(t, freezePath, 0)
 }