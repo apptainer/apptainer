sylabs/singularity

e2e: non-pid namespace e2e-tests for OCI/CGROUPS
The Singularity e2e-tests were previously all run in a mount and PID
namespace, to avoid polluting the host filesystem (critical), and
process tree (less critical). These namespaces are set up in some CGO
init code.

The use of the PID namespace prevents testing with systemd as the
cgroups manager, as systemd is aware of the host process tree, not the
one in the new e2e PID namespace. This is a major omission since #540
switched to using systemd for cgroups management by default.

This PR:

 * Modifies the e2e init CGO code, so that an env var
   `SINGULARITYE_E2E_NO_PID_NS` will cause it *not* to create a new PID
   namespace.

 * Modifies the Makefile so that the e2e suite is called twice, once
   with PID ns, once without.

 * Moves the INSTANCE cgroups test into a new package/topic
   e2e/cgroups, run in the e2e call without PID ns.

 * Moves the OCI tests into the e2e call without PID ns.

 * Modifies the CGROUPS and OCI tests so that they test with both
   systemd and cgroupfs management, using a convenience wrapper function.

Fixes: 563

Note - this breaks the e2e CLI coverage, as it only supports
collecting / analyzing one e2e run... where we now have two. The CLI
coverage metrics are unused in practice, and flawed (don't consider
combinations of flags required or possible), so I'm going to propose
removing them in a separate issue.

e2e/cgroups/cgroups.go
@@ -1,0 +1,89 @@
+// Copyright (c) 2022, Sylabs Inc. All rights reserved.
+// This software is licensed under a 3-clause BSD license. Please consult the
+// LICENSE.md file distributed with the sources of this project regarding your
+// rights to use or distribute this software.
+
+package cgroups
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/sylabs/singularity/e2e/internal/e2e"
+	"github.com/sylabs/singularity/e2e/internal/testhelper"
+	"github.com/sylabs/singularity/internal/pkg/test/tool/require"
+)
+
+//  NOTE
+//  ----
+//  Tests in this package/topic are run in a a mount namespace only. There is
+//  no PID namespace, in order that the systemd cgroups manager functionality
+//  can be exercised.
+//
+//  You must take extra care not to leave detached process etc. that will
+//  pollute the host PID namespace.
+//
+
+// randomName generates a random name instance or OCI container name based on a UUID.
+func randomName(t *testing.T) string {
+	t.Helper()
+
+	id, err := uuid.NewRandom()
+	if err != nil {
+		t.Fatal(err)
+	}
+	return id.String()
+}
+
+type ctx struct {
+	env e2e.TestEnv
+}
+
+// moved from INSTANCE suite, as testing with systemd cgroup manager requires
+// e2e to be run without PID namespace
+func (c *ctx) instanceApplyCgroups(t *testing.T) {
+	require.Cgroups(t)
+	e2e.EnsureImage(t, c.env)
+
+	// pick up a random name
+	instanceName := randomName(t)
+	joinName := fmt.Sprintf("instance://%s", instanceName)
+
+	c.env.RunSingularity(
+		t,
+		e2e.WithProfile(e2e.RootProfile),
+		e2e.WithCommand("instance start"),
+		e2e.WithArgs("--apply-cgroups", "testdata/cgroups/deny_device.toml", c.env.ImagePath, instanceName),
+		e2e.ExpectExit(0),
+	)
+
+	c.env.RunSingularity(
+		t,
+		e2e.WithProfile(e2e.RootProfile),
+		e2e.WithCommand("exec"),
+		e2e.WithArgs(joinName, "cat", "/dev/null"),
+		e2e.ExpectExit(1),
+	)
+
+	c.env.RunSingularity(
+		t,
+		e2e.WithProfile(e2e.RootProfile),
+		e2e.WithCommand("instance stop"),
+		e2e.WithArgs(instanceName),
+		e2e.ExpectExit(0),
+	)
+}
+
+// E2ETests is the main func to trigger the test suite
+func E2ETests(env e2e.TestEnv) testhelper.Tests {
+	c := &ctx{
+		env: env,
+	}
+
+	np := testhelper.NoParallel
+
+	return testhelper.Tests{
+		"instance apply cgroups": np(env.WithCgroupManagers(c.instanceApplyCgroups)),
+	}
+}

e2e/suite.go
@@ -1,5 +1,5 @@
 // Copyright (c) 2020, Control Command Inc. All rights reserved.
-// Copyright (c) 2019,2020 Sylabs Inc. All rights reserved.
+// Copyright (c) 2019-2022 Sylabs Inc. All rights reserved.
 // This software is licensed under a 3-clause BSD license. Please consult the
 // LICENSE.md file distributed with the sources of this project regarding your
 // rights to use or distribute this software.
@@ -22,6 +22,7 @@ import (
 	"github.com/sylabs/singularity/e2e/actions"
 	e2ebuildcfg "github.com/sylabs/singularity/e2e/buildcfg"
 	"github.com/sylabs/singularity/e2e/cache"
+	"github.com/sylabs/singularity/e2e/cgroups"
 	"github.com/sylabs/singularity/e2e/cmdenvvars"
 	"github.com/sylabs/singularity/e2e/config"
 	"github.com/sylabs/singularity/e2e/delete"
@@ -163,9 +164,15 @@ func Run(t *testing.T) {
 
 	suite := testhelper.NewSuite(t, testenv)
 
-	// RunE2ETests by functionality.
-	//
-	// Please keep this list sorted.
+	if os.Getenv("SINGULARITY_E2E_NO_PID_NS") != "" {
+		// e2e tests that will run in a mount namespace only
+		suite.AddGroup("CGROUPS", cgroups.E2ETests)
+		suite.AddGroup("OCI", oci.E2ETests)
+		suite.Run()
+		return
+	}
+
+	// e2e tests that will run in a mount and PID namespace
 	suite.AddGroup("ACTIONS", actions.E2ETests)
 	suite.AddGroup("BUILDCFG", e2ebuildcfg.E2ETests)
 	suite.AddGroup("BUILD", imgbuild.E2ETests)
@@ -181,7 +188,6 @@ func Run(t *testing.T) {
 	suite.AddGroup("INSPECT", inspect.E2ETests)
 	suite.AddGroup("INSTANCE", instance.E2ETests)
 	suite.AddGroup("KEY", key.E2ETests)
-	suite.AddGroup("OCI", oci.E2ETests)
 	suite.AddGroup("OVERLAY", overlay.E2ETests)
 	suite.AddGroup("PLUGIN", plugin.E2ETests)
 	suite.AddGroup("PULL", pull.E2ETests)
@@ -193,6 +199,5 @@ func Run(t *testing.T) {
 	suite.AddGroup("SIGN", sign.E2ETests)
 	suite.AddGroup("VERIFY", verify.E2ETests)
 	suite.AddGroup("VERSION", version.E2ETests)
-
 	suite.Run()
 }

apptainer/apptainer

Fixes: sylabs/singularity#563
Signed-off-by: Edita Kizinevic <edita.kizinevic@cern.ch>

e2e/cgroups/cgroups.go
@@ -1,0 +1,93 @@
+// Copyright (c) Contributors to the Apptainer project, established as
+//   Apptainer a Series of LF Projects LLC.
+//   For website terms of use, trademark policy, privacy policy and other
+//   project policies see https://lfprojects.org/policies
+// Copyright (c) 2022, Sylabs Inc. All rights reserved.
+// This software is licensed under a 3-clause BSD license. Please consult the
+// LICENSE.md file distributed with the sources of this project regarding your
+// rights to use or distribute this software.
+
+package cgroups
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/apptainer/apptainer/e2e/internal/e2e"
+	"github.com/apptainer/apptainer/e2e/internal/testhelper"
+	"github.com/apptainer/apptainer/internal/pkg/test/tool/require"
+	"github.com/google/uuid"
+)
+
+//  NOTE
+//  ----
+//  Tests in this package/topic are run in a a mount namespace only. There is
+//  no PID namespace, in order that the systemd cgroups manager functionality
+//  can be exercised.
+//
+//  You must take extra care not to leave detached process etc. that will
+//  pollute the host PID namespace.
+//
+
+// randomName generates a random name instance or OCI container name based on a UUID.
+func randomName(t *testing.T) string {
+	t.Helper()
+
+	id, err := uuid.NewRandom()
+	if err != nil {
+		t.Fatal(err)
+	}
+	return id.String()
+}
+
+type ctx struct {
+	env e2e.TestEnv
+}
+
+// moved from INSTANCE suite, as testing with systemd cgroup manager requires
+// e2e to be run without PID namespace
+func (c *ctx) instanceApplyCgroups(t *testing.T) {
+	require.Cgroups(t)
+	e2e.EnsureImage(t, c.env)
+
+	// pick up a random name
+	instanceName := randomName(t)
+	joinName := fmt.Sprintf("instance://%s", instanceName)
+
+	c.env.RunApptainer(
+		t,
+		e2e.WithProfile(e2e.RootProfile),
+		e2e.WithCommand("instance start"),
+		e2e.WithArgs("--apply-cgroups", "testdata/cgroups/deny_device.toml", c.env.ImagePath, instanceName),
+		e2e.ExpectExit(0),
+	)
+
+	c.env.RunApptainer(
+		t,
+		e2e.WithProfile(e2e.RootProfile),
+		e2e.WithCommand("exec"),
+		e2e.WithArgs(joinName, "cat", "/dev/null"),
+		e2e.ExpectExit(1),
+	)
+
+	c.env.RunApptainer(
+		t,
+		e2e.WithProfile(e2e.RootProfile),
+		e2e.WithCommand("instance stop"),
+		e2e.WithArgs(instanceName),
+		e2e.ExpectExit(0),
+	)
+}
+
+// E2ETests is the main func to trigger the test suite
+func E2ETests(env e2e.TestEnv) testhelper.Tests {
+	c := &ctx{
+		env: env,
+	}
+
+	np := testhelper.NoParallel
+
+	return testhelper.Tests{
+		"instance apply cgroups": np(env.WithCgroupManagers(c.instanceApplyCgroups)),
+	}
+}

e2e/suite.go
@@ -3,7 +3,7 @@
 //   For website terms of use, trademark policy, privacy policy and other
 //   project policies see https://lfprojects.org/policies
 // Copyright (c) 2020, Control Command Inc. All rights reserved.
-// Copyright (c) 2019,2020 Sylabs Inc. All rights reserved.
+// Copyright (c) 2019-2022 Sylabs Inc. All rights reserved.
 // This software is licensed under a 3-clause BSD license. Please consult the
 // LICENSE.md file distributed with the sources of this project regarding your
 // rights to use or distribute this software.
@@ -26,6 +26,7 @@ import (
 	"github.com/apptainer/apptainer/e2e/actions"
 	e2ebuildcfg "github.com/apptainer/apptainer/e2e/buildcfg"
 	"github.com/apptainer/apptainer/e2e/cache"
+	"github.com/apptainer/apptainer/e2e/cgroups"
 	"github.com/apptainer/apptainer/e2e/cmdenvvars"
 	"github.com/apptainer/apptainer/e2e/config"
 	"github.com/apptainer/apptainer/e2e/delete"
@@ -171,9 +172,15 @@ func Run(t *testing.T) {
 
 	suite := testhelper.NewSuite(t, testenv)
 
-	// RunE2ETests by functionality.
-	//
-	// Please keep this list sorted.
+	if os.Getenv("APPTAINER_E2E_NO_PID_NS") != "" {
+		// e2e tests that will run in a mount namespace only
+		suite.AddGroup("CGROUPS", cgroups.E2ETests)
+		suite.AddGroup("OCI", oci.E2ETests)
+		suite.Run()
+		return
+	}
+
+	// e2e tests that will run in a mount and PID namespace
 	suite.AddGroup("ACTIONS", actions.E2ETests)
 	suite.AddGroup("BUILDCFG", e2ebuildcfg.E2ETests)
 	suite.AddGroup("BUILD", imgbuild.E2ETests)
@@ -190,7 +197,6 @@ func Run(t *testing.T) {
 	suite.AddGroup("INSTANCE", instance.E2ETests)
 	suite.AddGroup("KEY", key.E2ETests)
 	suite.AddGroup("LEGACY", legacy.E2ETests)
-	suite.AddGroup("OCI", oci.E2ETests)
 	suite.AddGroup("OVERLAY", overlay.E2ETests)
 	suite.AddGroup("PLUGIN", plugin.E2ETests)
 	suite.AddGroup("PULL", pull.E2ETests)
@@ -202,6 +208,5 @@ func Run(t *testing.T) {
 	suite.AddGroup("SIGN", sign.E2ETests)
 	suite.AddGroup("VERIFY", verify.E2ETests)
 	suite.AddGroup("VERSION", version.E2ETests)
-
 	suite.Run()
 }