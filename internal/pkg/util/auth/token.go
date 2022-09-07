ddd78fc11ec484864f2d3ede36985a1606a0a0f9
Adam Hughes <9903835+tri-adam@users.noreply.github.com>
2022-09-07 21:50:00 +0000

    refactor: replace use of deprecated ioutil.ReadFile
    Signed-off-by: Edita Kizinevic <edita.kizinevic@cern.ch>

--- internal/pkg/util/auth/token.go
+++ internal/pkg/util/auth/token.go
@@ -2,13 +2,10 @@
 //   Apptainer a Series of LF Projects LLC.
 //   For website terms of use, trademark policy, privacy policy and other
 //   project policies see https://lfprojects.org/policies
-/*
-  Copyright (c) 2018, Sylabs, Inc. All rights reserved.
-
-  This software is licensed under a 3-clause BSD license.  Please
-  consult LICENSE.md file distributed with the sources of this project regarding
-  your rights to use or distribute this software.
-*/
+// Copyright (c) 2018-2022, Sylabs Inc. All rights reserved.
+// This software is licensed under a 3-clause BSD license. Please consult the
+// LICENSE.md file distributed with the sources of this project regarding your
+// rights to use or distribute this software.
 
 package auth
 
