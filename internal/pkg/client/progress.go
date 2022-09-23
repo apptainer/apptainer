c7bc034cfae793597e74a5f6b066ddb3bb7b18f3
Adam Hughes <9903835+tri-adam@users.noreply.github.com>
2022-09-23 17:20:32 +0000

    fix: silence contextcheck for initProgressBar usage
    Signed-off-by: Edita Kizinevic <edita.kizinevic@cern.ch>

--- internal/pkg/client/progress.go
+++ internal/pkg/client/progress.go
@@ -2,7 +2,7 @@
 //   Apptainer a Series of LF Projects LLC.
 //   For website terms of use, trademark policy, privacy policy and other
 //   project policies see https://lfprojects.org/policies
-// Copyright (c) 2018-2021, Sylabs Inc. All rights reserved.
+// Copyright (c) 2018-2022, Sylabs Inc. All rights reserved.
 // This software is licensed under a 3-clause BSD license. Please consult the
 // LICENSE.md file distributed with the sources of this project regarding your
 // rights to use or distribute this software.
@@ -61,7 +61,7 @@ func ProgressBarCallback(ctx context.Context) ProgressCallback {
 	}
 
 	return func(totalSize int64, r io.Reader, w io.Writer) error {
-		p, bar := initProgressBar(totalSize)
+		p, bar := initProgressBar(totalSize) //nolint:contextcheck
 
 		// create proxy reader
 		bodyProgress := bar.ProxyReader(r)
