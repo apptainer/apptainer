sylabs/singularity

feat: add env var for '--key' sign option

e2e/sign/sign.go
@@ -216,6 +216,23 @@ func (c ctx) singularitySignKeyOption(t *testing.T) {
 	)
 }
 
+func (c ctx) singularitySignKeyEnv(t *testing.T) {
+	imgPath, cleanup := c.prepareImage(t)
+	defer cleanup(t)
+
+	c.env.RunSingularity(
+		t,
+		e2e.WithProfile(e2e.UserProfile),
+		e2e.WithEnv([]string{"SINGULARITY_SIGN_KEY=" + filepath.Join("..", "test", "keys", "private.pem")}),
+		e2e.WithCommand("sign"),
+		e2e.WithArgs(imgPath),
+		e2e.ExpectExit(
+			0,
+			e2e.ExpectOutput(e2e.ContainMatch, "Signature created and applied to "+imgPath),
+		),
+	)
+}
+
 func (c *ctx) generateKeypair(t *testing.T) {
 	keyGenInput := []e2e.SingularityConsoleOp{
 		e2e.ConsoleSendLine("e2e sign test key"),
@@ -268,6 +285,7 @@ func E2ETests(env e2e.TestEnv) testhelper.Tests {
 			t.Run("singularitySignGroupIDOption", c.singularitySignGroupIDOption)
 			t.Run("singularitySignKeyidxOption", c.singularitySignKeyidxOption)
 			t.Run("singularitySignKeyOption", c.singularitySignKeyOption)
+			t.Run("singularitySignKeyEnv", c.singularitySignKeyEnv)
 		},
 	}
 }

apptainer/apptainer

Signed-off-by: Edita Kizinevic <edita.kizinevic@cern.ch>

e2e/sign/sign.go
@@ -220,6 +220,23 @@ func (c ctx) apptainerSignKeyOption(t *testing.T) {
 	)
 }
 
+func (c ctx) apptainerSignKeyEnv(t *testing.T) {
+	imgPath, cleanup := c.prepareImage(t)
+	defer cleanup(t)
+
+	c.env.RunApptainer(
+		t,
+		e2e.WithProfile(e2e.UserProfile),
+		e2e.WithEnv([]string{"APPTAINER_SIGN_KEY=" + filepath.Join("..", "test", "keys", "private.pem")}),
+		e2e.WithCommand("sign"),
+		e2e.WithArgs(imgPath),
+		e2e.ExpectExit(
+			0,
+			e2e.ExpectOutput(e2e.ContainMatch, "Signature created and applied to "+imgPath),
+		),
+	)
+}
+
 func (c *ctx) generateKeypair(t *testing.T) {
 	keyGenInput := []e2e.ApptainerConsoleOp{
 		e2e.ConsoleSendLine("e2e sign test key"),
@@ -272,6 +289,7 @@ func E2ETests(env e2e.TestEnv) testhelper.Tests {
 			t.Run("apptainerSignGroupIDOption", c.apptainerSignGroupIDOption)
 			t.Run("apptainerSignKeyidxOption", c.apptainerSignKeyidxOption)
 			t.Run("apptainerSignKeyOption", c.apptainerSignKeyOption)
+			t.Run("apptainerSignKeyEnv", c.apptainerSignKeyEnv)
 		},
 	}
 }