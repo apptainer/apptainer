tri-adam/singularity

feat: implement X.509 support for verify command

cmd/internal/cli/verify.go
@@ -21,13 +21,16 @@ import (
 )
 
 var (
-	sifGroupID   uint32 // -g groupid specification
-	sifDescID    uint32 // -i id specification
-	pubKeyPath   string // --key flag
-	localVerify  bool   // -l flag
-	jsonVerify   bool   // -j flag
-	verifyAll    bool
-	verifyLegacy bool
+	sifGroupID                   uint32 // -g groupid specification
+	sifDescID                    uint32 // -i id specification
+	certificatePath              string // --certificate flag
+	certificateIntermediatesPath string // --certificate-intermediates flag
+	certificateRootsPath         string // --certificate-roots flag
+	pubKeyPath                   string // --key flag
+	localVerify                  bool   // -l flag
+	jsonVerify                   bool   // -j flag
+	verifyAll                    bool
+	verifyLegacy                 bool
 )
 
 // -u|--url
@@ -79,6 +82,33 @@ var verifySifDescIDFlag = cmdline.Flag{
 	Name:         "id",
 	Usage:        "verify object with the specified ID",
 	Deprecated:   "use '--sif-id'",
+}
+
+// --certificate
+var verifyCertificateFlag = cmdline.Flag{
+	ID:           "certificateFlag",
+	Value:        &certificatePath,
+	DefaultValue: "",
+	Name:         "certificate",
+	Usage:        "path to the certificate",
+}
+
+// --certificate-intermediates
+var verifyCertificateIntermediatesFlag = cmdline.Flag{
+	ID:           "certificateIntermediatesFlag",
+	Value:        &certificateIntermediatesPath,
+	DefaultValue: "",
+	Name:         "certificate-intermediates",
+	Usage:        "path to pool of intermediate certificates",
+}
+
+// --certificate-roots
+var verifyCertificateRootsFlag = cmdline.Flag{
+	ID:           "certificateRootsFlag",
+	Value:        &certificateRootsPath,
+	DefaultValue: "",
+	Name:         "certificate-roots",
+	Usage:        "path to pool of root certificates",
 }
 
 // --key
@@ -139,6 +169,9 @@ func init() {
 		cmdManager.RegisterFlagForCmd(&verifyOldSifGroupIDFlag, VerifyCmd)
 		cmdManager.RegisterFlagForCmd(&verifySifDescSifIDFlag, VerifyCmd)
 		cmdManager.RegisterFlagForCmd(&verifySifDescIDFlag, VerifyCmd)
+		cmdManager.RegisterFlagForCmd(&verifyCertificateFlag, VerifyCmd)
+		cmdManager.RegisterFlagForCmd(&verifyCertificateIntermediatesFlag, VerifyCmd)
+		cmdManager.RegisterFlagForCmd(&verifyCertificateRootsFlag, VerifyCmd)
 		cmdManager.RegisterFlagForCmd(&verifyPublicKeyFlag, VerifyCmd)
 		cmdManager.RegisterFlagForCmd(&verifyLocalFlag, VerifyCmd)
 		cmdManager.RegisterFlagForCmd(&verifyJSONFlag, VerifyCmd)
@@ -167,6 +200,29 @@ func doVerifyCmd(cmd *cobra.Command, cpath string) {
 	var opts []singularity.VerifyOpt
 
 	switch {
+	case cmd.Flag(verifyCertificateFlag.Name).Changed:
+		c, err := loadCertificate(certificatePath)
+		if err != nil {
+			sylog.Fatalf("Failed to load certificate: %v", err)
+		}
+		opts = append(opts, singularity.OptVerifyWithCertificate(c))
+
+		if cmd.Flag(verifyCertificateIntermediatesFlag.Name).Changed {
+			p, err := loadCertificatePool(certificateIntermediatesPath)
+			if err != nil {
+				sylog.Fatalf("Failed to load intermediate certificates: %v", err)
+			}
+			opts = append(opts, singularity.OptVerifyWithIntermediates(p))
+		}
+
+		if cmd.Flag(verifyCertificateRootsFlag.Name).Changed {
+			p, err := loadCertificatePool(certificateRootsPath)
+			if err != nil {
+				sylog.Fatalf("Failed to load root certificates: %v", err)
+			}
+			opts = append(opts, singularity.OptVerifyWithRoots(p))
+		}
+
 	case cmd.Flag(verifyPublicKeyFlag.Name).Changed:
 		v, err := signature.LoadVerifierFromPEMFile(pubKeyPath, crypto.SHA256)
 		if err != nil {

apptainer/apptainer

Signed-off-by: Edita Kizinevic <edita.kizinevic@cern.ch>

cmd/internal/cli/verify.go
@@ -24,13 +24,16 @@ import (
 )
 
 var (
-	sifGroupID   uint32 // -g groupid specification
-	sifDescID    uint32 // -i id specification
-	pubKeyPath   string // --key flag
-	localVerify  bool   // -l flag
-	jsonVerify   bool   // -j flag
-	verifyAll    bool
-	verifyLegacy bool
+	sifGroupID                   uint32 // -g groupid specification
+	sifDescID                    uint32 // -i id specification
+	certificatePath              string // --certificate flag
+	certificateIntermediatesPath string // --certificate-intermediates flag
+	certificateRootsPath         string // --certificate-roots flag
+	pubKeyPath                   string // --key flag
+	localVerify                  bool   // -l flag
+	jsonVerify                   bool   // -j flag
+	verifyAll                    bool
+	verifyLegacy                 bool
 )
 
 // -u|--url
@@ -82,6 +85,36 @@ var verifySifDescIDFlag = cmdline.Flag{
 	Name:         "id",
 	Usage:        "verify object with the specified ID",
 	Deprecated:   "use '--sif-id'",
+}
+
+// --certificate
+var verifyCertificateFlag = cmdline.Flag{
+	ID:           "certificateFlag",
+	Value:        &certificatePath,
+	DefaultValue: "",
+	Name:         "certificate",
+	Usage:        "path to the certificate",
+	EnvKeys:      []string{"VERIFY_CERTIFICATE"},
+}
+
+// --certificate-intermediates
+var verifyCertificateIntermediatesFlag = cmdline.Flag{
+	ID:           "certificateIntermediatesFlag",
+	Value:        &certificateIntermediatesPath,
+	DefaultValue: "",
+	Name:         "certificate-intermediates",
+	Usage:        "path to pool of intermediate certificates",
+	EnvKeys:      []string{"VERIFY_INTERMEDIATES"},
+}
+
+// --certificate-roots
+var verifyCertificateRootsFlag = cmdline.Flag{
+	ID:           "certificateRootsFlag",
+	Value:        &certificateRootsPath,
+	DefaultValue: "",
+	Name:         "certificate-roots",
+	Usage:        "path to pool of root certificates",
+	EnvKeys:      []string{"VERIFY_ROOTS"},
 }
 
 // --key
@@ -143,6 +176,9 @@ func init() {
 		cmdManager.RegisterFlagForCmd(&verifyOldSifGroupIDFlag, VerifyCmd)
 		cmdManager.RegisterFlagForCmd(&verifySifDescSifIDFlag, VerifyCmd)
 		cmdManager.RegisterFlagForCmd(&verifySifDescIDFlag, VerifyCmd)
+		cmdManager.RegisterFlagForCmd(&verifyCertificateFlag, VerifyCmd)
+		cmdManager.RegisterFlagForCmd(&verifyCertificateIntermediatesFlag, VerifyCmd)
+		cmdManager.RegisterFlagForCmd(&verifyCertificateRootsFlag, VerifyCmd)
 		cmdManager.RegisterFlagForCmd(&verifyPublicKeyFlag, VerifyCmd)
 		cmdManager.RegisterFlagForCmd(&verifyLocalFlag, VerifyCmd)
 		cmdManager.RegisterFlagForCmd(&verifyJSONFlag, VerifyCmd)
@@ -171,6 +207,31 @@ func doVerifyCmd(cmd *cobra.Command, cpath string) {
 	var opts []apptainer.VerifyOpt
 
 	switch {
+	case cmd.Flag(verifyCertificateFlag.Name).Changed:
+		sylog.Infof("Verifying image with key material from certificate '%v'", certificatePath)
+
+		c, err := loadCertificate(certificatePath)
+		if err != nil {
+			sylog.Fatalf("Failed to load certificate: %v", err)
+		}
+		opts = append(opts, apptainer.OptVerifyWithCertificate(c))
+
+		if cmd.Flag(verifyCertificateIntermediatesFlag.Name).Changed {
+			p, err := loadCertificatePool(certificateIntermediatesPath)
+			if err != nil {
+				sylog.Fatalf("Failed to load intermediate certificates: %v", err)
+			}
+			opts = append(opts, apptainer.OptVerifyWithIntermediates(p))
+		}
+
+		if cmd.Flag(verifyCertificateRootsFlag.Name).Changed {
+			p, err := loadCertificatePool(certificateRootsPath)
+			if err != nil {
+				sylog.Fatalf("Failed to load root certificates: %v", err)
+			}
+			opts = append(opts, apptainer.OptVerifyWithRoots(p))
+		}
+
 	case cmd.Flag(verifyPublicKeyFlag.Name).Changed:
 		sylog.Infof("Verifying image with key material from '%v'", pubKeyPath)
 