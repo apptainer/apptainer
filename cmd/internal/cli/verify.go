sylabs/singularity

Add support for OCSP verification
Signed-off-by: Fotis Nikolaidis <nikolaidis.fotis@gmail.com>

cmd/internal/cli/verify.go
@@ -25,6 +25,7 @@ var (
 	certificatePath              string // --certificate flag
 	certificateIntermediatesPath string // --certificate-intermediates flag
 	certificateRootsPath         string // --certificate-roots flag
+	ocspVerify                   bool   // --ocsp-verify flag
 	pubKeyPath                   string // --key flag
 	localVerify                  bool   // -l flag
 	jsonVerify                   bool   // -j flag
@@ -113,6 +114,16 @@ var verifyCertificateRootsFlag = cmdline.Flag{
 	EnvKeys:      []string{"VERIFY_ROOTS"},
 }
 
+// --ocsp-verify
+var verifyOCSPFlag = cmdline.Flag{
+	ID:           "ocspVerifyFlag",
+	Value:        &ocspVerify,
+	DefaultValue: false,
+	Name:         "ocsp-verify",
+	Usage:        "enable online revocation check for certificates",
+	EnvKeys:      []string{"VERIFY_OCSP"},
+}
+
 // --key
 var verifyPublicKeyFlag = cmdline.Flag{
 	ID:           "publicKeyFlag",
@@ -175,6 +186,7 @@ func init() {
 		cmdManager.RegisterFlagForCmd(&verifyCertificateFlag, VerifyCmd)
 		cmdManager.RegisterFlagForCmd(&verifyCertificateIntermediatesFlag, VerifyCmd)
 		cmdManager.RegisterFlagForCmd(&verifyCertificateRootsFlag, VerifyCmd)
+		cmdManager.RegisterFlagForCmd(&verifyOCSPFlag, VerifyCmd)
 		cmdManager.RegisterFlagForCmd(&verifyPublicKeyFlag, VerifyCmd)
 		cmdManager.RegisterFlagForCmd(&verifyLocalFlag, VerifyCmd)
 		cmdManager.RegisterFlagForCmd(&verifyJSONFlag, VerifyCmd)
@@ -226,6 +238,10 @@ func doVerifyCmd(cmd *cobra.Command, cpath string) {
 				sylog.Fatalf("Failed to load root certificates: %v", err)
 			}
 			opts = append(opts, singularity.OptVerifyWithRoots(p))
+		}
+
+		if cmd.Flag(verifyOCSPFlag.Name).Changed {
+			opts = append(opts, singularity.OptVerifyWithOCSP())
 		}
 
 	case cmd.Flag(verifyPublicKeyFlag.Name).Changed:

e2e/verify/ocspresponder/standalone/main.go
@@ -1,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/sylabs/singularity/e2e/verify/ocspresponder"
+)
+
+func main() {
+	if err := ocspresponder.StartOCSPResponder(ocspresponder.DefaultOCSPResponderArgs); err != nil {
+		fmt.Fprintln(os.Stderr, "Error:", err)
+		os.Exit(1)
+	}
+}

e2e/verify/verify.go
@@ -6,9 +6,13 @@
 package verify
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 	"testing"
+	"time"
+
+	"github.com/sylabs/singularity/e2e/verify/ocspresponder"
 
 	"github.com/sylabs/singularity/e2e/internal/e2e"
 	"github.com/sylabs/singularity/e2e/internal/testhelper"
@@ -19,10 +23,14 @@ type ctx struct {
 }
 
 func (c *ctx) verify(t *testing.T) {
-	keyPath := filepath.Join("..", "test", "keys", "ed25519-public.pem")
+	pubKeyPath := filepath.Join("..", "test", "keys", "ed25519-public.pem")
+	priKeyPath := filepath.Join("..", "test", "keys", "ed25519-private.pem")
+
 	certPath := filepath.Join("..", "test", "certs", "leaf.pem")
 	intPath := filepath.Join("..", "test", "certs", "intermediate.pem")
 	rootPath := filepath.Join("..", "test", "certs", "root.pem")
+
+	c.startOCSPResponder(priKeyPath, rootPath)
 
 	tests := []struct {
 		name       string
@@ -113,19 +121,19 @@ func (c *ctx) verify(t *testing.T) {
 		},
 		{
 			name:      "KeyFlag",
-			flags:     []string{"--key", keyPath},
+			flags:     []string{"--key", pubKeyPath},
 			imagePath: filepath.Join("..", "test", "images", "one-group-signed-dsse.sif"),
 			expectOps: []e2e.SingularityCmdResultOp{
-				e2e.ExpectError(e2e.ContainMatch, "Verifying image with key material from '"+keyPath+"'"),
+				e2e.ExpectError(e2e.ContainMatch, "Verifying image with key material from '"+pubKeyPath+"'"),
 				e2e.ExpectError(e2e.ContainMatch, "Verified signature(s) from image"),
 			},
 		},
 		{
 			name:      "KeyEnvVar",
-			envs:      []string{"SINGULARITY_VERIFY_KEY=" + keyPath},
+			envs:      []string{"SINGULARITY_VERIFY_KEY=" + pubKeyPath},
 			imagePath: filepath.Join("..", "test", "images", "one-group-signed-dsse.sif"),
 			expectOps: []e2e.SingularityCmdResultOp{
-				e2e.ExpectError(e2e.ContainMatch, "Verifying image with key material from '"+keyPath+"'"),
+				e2e.ExpectError(e2e.ContainMatch, "Verifying image with key material from '"+pubKeyPath+"'"),
 				e2e.ExpectError(e2e.ContainMatch, "Verified signature(s) from image"),
 			},
 		},
@@ -155,6 +163,52 @@ func (c *ctx) verify(t *testing.T) {
 				e2e.ExpectError(e2e.ContainMatch, "Verified signature(s) from image"),
 			},
 		},
+		{
+			name: "OCSPFlags",
+			flags: []string{
+				"--certificate", certPath,
+				"--certificate-intermediates", intPath,
+				"--certificate-roots", rootPath,
+				"--ocsp-verify",
+			},
+			imagePath:  filepath.Join("..", "test", "images", "one-group-signed-dsse.sif"),
+			expectCode: 255,
+			expectOps: []e2e.SingularityCmdResultOp{
+				// Expect OCSP to fail due to https://github.com/sylabs/singularity/issues/1152
+				e2e.ExpectError(e2e.ContainMatch, "Failed to verify container: OCSP verification has failed"),
+			},
+		},
+		{
+			name: "OCSPEnvVars",
+			envs: []string{
+				"SINGULARITY_VERIFY_CERTIFICATE=" + certPath,
+				"SINGULARITY_VERIFY_INTERMEDIATES=" + intPath,
+				"SINGULARITY_VERIFY_ROOTS=" + rootPath,
+				"SINGULARITY_VERIFY_OCSP=true",
+			},
+			imagePath:  filepath.Join("..", "test", "images", "one-group-signed-dsse.sif"),
+			expectCode: 255,
+			expectOps: []e2e.SingularityCmdResultOp{
+				// Expect OCSP to fail due to https://github.com/sylabs/singularity/issues/1152
+				e2e.ExpectError(e2e.ContainMatch, "Failed to verify container: OCSP verification has failed"),
+			},
+		},
+		{
+			name: "OCSPThirdPartyChain",
+			flags: []string{
+				"--certificate", filepath.Join("./verify", "ocspcertificates", "leaf.pem"),
+				"--certificate-intermediates", filepath.Join("./verify", "ocspcertificates", "intermediate.pem"),
+				"--ocsp-verify",
+			},
+			imagePath:  filepath.Join("..", "test", "images", "one-group-signed-dsse.sif"),
+			expectCode: 255,
+			expectOps: []e2e.SingularityCmdResultOp{
+				e2e.ExpectError(e2e.ContainMatch, "Failed to verify container: x509: certificate specifies an incompatible key usage"),
+				// https://github.com/sylabs/singularity/pull/1213#pullrequestreview-1240524316
+				// Error Expect OCSP to succeed, but signature verification to fail.
+				// e2e.ExpectError(e2e.ContainMatch, "Failed to verify container: integrity: signature object 3 not valid: dsse: verify envelope failed: Accepted signatures do not match threshold, Found: 0, Expected 1"),
+			},
+		},
 	}
 
 	for _, tt := range tests {
@@ -177,6 +231,25 @@ func (c *ctx) importPGPKeypairs(t *testing.T) {
 		e2e.WithArgs(filepath.Join("..", "test", "keys", "pgp-public.asc")),
 		e2e.ExpectExit(0),
 	)
+}
+
+func (c *ctx) startOCSPResponder(rootKeyPath string, rootCertPath string) {
+	// initiate OCSP responder to validate the singularity certificate chain
+	go func() {
+		args := ocspresponder.ResponderArgs{
+			IndexFile:    filepath.Join("./verify", "ocspresponder", "index.txt"),
+			ServerPort:   "9999",
+			OCSPKeyPath:  rootKeyPath,
+			OCSPCertPath: rootCertPath,
+			CACertPath:   rootCertPath,
+		}
+
+		if err := ocspresponder.StartOCSPResponder(args); err != nil {
+			panic(fmt.Errorf("responder initialization has failed due to '%s'", err))
+		}
+	}()
+
+	time.Sleep(5 * time.Second)
 }
 
 // E2ETests is the main func to trigger the test suite

apptainer/apptainer

Signed-off-by: Edita Kizinevic <edita.kizinevic@cern.ch>

cmd/internal/cli/verify.go
@@ -29,6 +29,7 @@ var (
 	certificatePath              string // --certificate flag
 	certificateIntermediatesPath string // --certificate-intermediates flag
 	certificateRootsPath         string // --certificate-roots flag
+	ocspVerify                   bool   // --ocsp-verify flag
 	pubKeyPath                   string // --key flag
 	localVerify                  bool   // -l flag
 	jsonVerify                   bool   // -j flag
@@ -117,6 +118,16 @@ var verifyCertificateRootsFlag = cmdline.Flag{
 	EnvKeys:      []string{"VERIFY_ROOTS"},
 }
 
+// --ocsp-verify
+var verifyOCSPFlag = cmdline.Flag{
+	ID:           "ocspVerifyFlag",
+	Value:        &ocspVerify,
+	DefaultValue: false,
+	Name:         "ocsp-verify",
+	Usage:        "enable online revocation check for certificates",
+	EnvKeys:      []string{"VERIFY_OCSP"},
+}
+
 // --key
 var verifyPublicKeyFlag = cmdline.Flag{
 	ID:           "publicKeyFlag",
@@ -179,6 +190,7 @@ func init() {
 		cmdManager.RegisterFlagForCmd(&verifyCertificateFlag, VerifyCmd)
 		cmdManager.RegisterFlagForCmd(&verifyCertificateIntermediatesFlag, VerifyCmd)
 		cmdManager.RegisterFlagForCmd(&verifyCertificateRootsFlag, VerifyCmd)
+		cmdManager.RegisterFlagForCmd(&verifyOCSPFlag, VerifyCmd)
 		cmdManager.RegisterFlagForCmd(&verifyPublicKeyFlag, VerifyCmd)
 		cmdManager.RegisterFlagForCmd(&verifyLocalFlag, VerifyCmd)
 		cmdManager.RegisterFlagForCmd(&verifyJSONFlag, VerifyCmd)
@@ -230,6 +242,10 @@ func doVerifyCmd(cmd *cobra.Command, cpath string) {
 				sylog.Fatalf("Failed to load root certificates: %v", err)
 			}
 			opts = append(opts, apptainer.OptVerifyWithRoots(p))
+		}
+
+		if cmd.Flag(verifyOCSPFlag.Name).Changed {
+			opts = append(opts, apptainer.OptVerifyWithOCSP())
 		}
 
 	case cmd.Flag(verifyPublicKeyFlag.Name).Changed:

e2e/verify/ocspresponder/standalone/main.go
@@ -1,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/apptainer/apptainer/e2e/verify/ocspresponder"
+)
+
+func main() {
+	if err := ocspresponder.StartOCSPResponder(ocspresponder.DefaultOCSPResponderArgs); err != nil {
+		fmt.Fprintln(os.Stderr, "Error:", err)
+		os.Exit(1)
+	}
+}

e2e/verify/verify.go
@@ -10,9 +10,13 @@
 package verify
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 	"testing"
+	"time"
+
+	"github.com/apptainer/apptainer/e2e/verify/ocspresponder"
 
 	"github.com/apptainer/apptainer/e2e/internal/e2e"
 	"github.com/apptainer/apptainer/e2e/internal/testhelper"
@@ -23,10 +27,14 @@ type ctx struct {
 }
 
 func (c *ctx) verify(t *testing.T) {
-	keyPath := filepath.Join("..", "test", "keys", "ed25519-public.pem")
+	pubKeyPath := filepath.Join("..", "test", "keys", "ed25519-public.pem")
+	priKeyPath := filepath.Join("..", "test", "keys", "ed25519-private.pem")
+
 	certPath := filepath.Join("..", "test", "certs", "leaf.pem")
 	intPath := filepath.Join("..", "test", "certs", "intermediate.pem")
 	rootPath := filepath.Join("..", "test", "certs", "root.pem")
+
+	c.startOCSPResponder(priKeyPath, rootPath)
 
 	tests := []struct {
 		name       string
@@ -117,19 +125,19 @@ func (c *ctx) verify(t *testing.T) {
 		},
 		{
 			name:      "KeyFlag",
-			flags:     []string{"--key", keyPath},
+			flags:     []string{"--key", pubKeyPath},
 			imagePath: filepath.Join("..", "test", "images", "one-group-signed-dsse.sif"),
 			expectOps: []e2e.ApptainerCmdResultOp{
-				e2e.ExpectError(e2e.ContainMatch, "Verifying image with key material from '"+keyPath+"'"),
+				e2e.ExpectError(e2e.ContainMatch, "Verifying image with key material from '"+pubKeyPath+"'"),
 				e2e.ExpectError(e2e.ContainMatch, "Verified signature(s) from image"),
 			},
 		},
 		{
 			name:      "KeyEnvVar",
-			envs:      []string{"APPTAINER_VERIFY_KEY=" + keyPath},
+			envs:      []string{"APPTAINER_VERIFY_KEY=" + pubKeyPath},
 			imagePath: filepath.Join("..", "test", "images", "one-group-signed-dsse.sif"),
 			expectOps: []e2e.ApptainerCmdResultOp{
-				e2e.ExpectError(e2e.ContainMatch, "Verifying image with key material from '"+keyPath+"'"),
+				e2e.ExpectError(e2e.ContainMatch, "Verifying image with key material from '"+pubKeyPath+"'"),
 				e2e.ExpectError(e2e.ContainMatch, "Verified signature(s) from image"),
 			},
 		},
@@ -159,6 +167,52 @@ func (c *ctx) verify(t *testing.T) {
 				e2e.ExpectError(e2e.ContainMatch, "Verified signature(s) from image"),
 			},
 		},
+		{
+			name: "OCSPFlags",
+			flags: []string{
+				"--certificate", certPath,
+				"--certificate-intermediates", intPath,
+				"--certificate-roots", rootPath,
+				"--ocsp-verify",
+			},
+			imagePath:  filepath.Join("..", "test", "images", "one-group-signed-dsse.sif"),
+			expectCode: 255,
+			expectOps: []e2e.ApptainerCmdResultOp{
+				// Expect OCSP to fail due to https://github.com/sylabs/singularity/issues/1152
+				e2e.ExpectError(e2e.ContainMatch, "Failed to verify container: OCSP verification has failed"),
+			},
+		},
+		{
+			name: "OCSPEnvVars",
+			envs: []string{
+				"APPTAINER_VERIFY_CERTIFICATE=" + certPath,
+				"APPTAINER_VERIFY_INTERMEDIATES=" + intPath,
+				"APPTAINER_VERIFY_ROOTS=" + rootPath,
+				"APPTAINER_VERIFY_OCSP=true",
+			},
+			imagePath:  filepath.Join("..", "test", "images", "one-group-signed-dsse.sif"),
+			expectCode: 255,
+			expectOps: []e2e.ApptainerCmdResultOp{
+				// Expect OCSP to fail due to https://github.com/sylabs/singularity/issues/1152
+				e2e.ExpectError(e2e.ContainMatch, "Failed to verify container: OCSP verification has failed"),
+			},
+		},
+		{
+			name: "OCSPThirdPartyChain",
+			flags: []string{
+				"--certificate", filepath.Join("./verify", "ocspcertificates", "leaf.pem"),
+				"--certificate-intermediates", filepath.Join("./verify", "ocspcertificates", "intermediate.pem"),
+				"--ocsp-verify",
+			},
+			imagePath:  filepath.Join("..", "test", "images", "one-group-signed-dsse.sif"),
+			expectCode: 255,
+			expectOps: []e2e.ApptainerCmdResultOp{
+				e2e.ExpectError(e2e.ContainMatch, "Failed to verify container: x509: certificate specifies an incompatible key usage"),
+				// https://github.com/sylabs/singularity/pull/1213#pullrequestreview-1240524316
+				// Error Expect OCSP to succeed, but signature verification to fail.
+				// e2e.ExpectError(e2e.ContainMatch, "Failed to verify container: integrity: signature object 3 not valid: dsse: verify envelope failed: Accepted signatures do not match threshold, Found: 0, Expected 1"),
+			},
+		},
 	}
 
 	for _, tt := range tests {
@@ -181,6 +235,25 @@ func (c *ctx) importPGPKeypairs(t *testing.T) {
 		e2e.WithArgs(filepath.Join("..", "test", "keys", "pgp-public.asc")),
 		e2e.ExpectExit(0),
 	)
+}
+
+func (c *ctx) startOCSPResponder(rootKeyPath string, rootCertPath string) {
+	// initiate OCSP responder to validate the apptainer certificate chain
+	go func() {
+		args := ocspresponder.ResponderArgs{
+			IndexFile:    filepath.Join("./verify", "ocspresponder", "index.txt"),
+			ServerPort:   "9999",
+			OCSPKeyPath:  rootKeyPath,
+			OCSPCertPath: rootCertPath,
+			CACertPath:   rootCertPath,
+		}
+
+		if err := ocspresponder.StartOCSPResponder(args); err != nil {
+			panic(fmt.Errorf("responder initialization has failed due to '%s'", err))
+		}
+	}()
+
+	time.Sleep(5 * time.Second)
 }
 
 // E2ETests is the main func to trigger the test suite