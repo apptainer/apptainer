sylabs/singularity

chore: Move i/p/a/singularity Sign/Verify functions to separate pkg
Implementation of SIF signing and verification was previously part of
the large internal/pkg/app/singularity package.

This situation is problematic, as verification may need to be
performed in builds, client pulls etc. Consquently
internal/pkg/app/singularity had to be imported in lower level code.

Work on the OCI mode has exposed situations where this can result in
import loops.

After recent re-writes, the Sign / Verify functions are not tied to
other portions of internal/pkg/app/singularity. Therefore this PR
moves them to a separate package to avoid import loops etc.

As a result, internal/pkg/app/singularity is now only imported by cmd/.

cmd/internal/cli/pgp.go
@@ -21,7 +21,7 @@ import (
 	"github.com/fatih/color"
 	"github.com/sylabs/sif/v2/pkg/integrity"
 	"github.com/sylabs/sif/v2/pkg/sif"
-	"github.com/sylabs/singularity/internal/app/singularity"
+	sifsignature "github.com/sylabs/singularity/internal/pkg/signature"
 	"github.com/sylabs/singularity/internal/pkg/util/interactive"
 	"github.com/sylabs/singularity/pkg/sylog"
 	"github.com/sylabs/singularity/pkg/sypgp"
@@ -220,8 +220,8 @@ type keyList struct {
 	SignerKeys []*key
 }
 
-// getJSONCallback returns a singularity.VerifyCallback that appends to kl.
-func getJSONCallback(kl *keyList) singularity.VerifyCallback {
+// getJSONCallback returns a signature.VerifyCallback that appends to kl.
+func getJSONCallback(kl *keyList) sifsignature.VerifyCallback {
 	return func(f *sif.FileImage, r integrity.VerifyResult) bool {
 		name, fp := "unknown", ""
 		var keyLocal, keyCheck bool

cmd/internal/cli/sign.go
@@ -12,7 +12,7 @@ import (
 	"github.com/sigstore/sigstore/pkg/signature"
 	"github.com/spf13/cobra"
 	"github.com/sylabs/singularity/docs"
-	"github.com/sylabs/singularity/internal/app/singularity"
+	sifsignature "github.com/sylabs/singularity/internal/pkg/signature"
 	"github.com/sylabs/singularity/pkg/cmdline"
 	"github.com/sylabs/singularity/pkg/sylog"
 	"github.com/sylabs/singularity/pkg/sypgp"
@@ -126,7 +126,7 @@ var SignCmd = &cobra.Command{
 }
 
 func doSignCmd(cmd *cobra.Command, cpath string) {
-	var opts []singularity.SignOpt
+	var opts []sifsignature.SignOpt
 
 	// Set key material.
 	switch {
@@ -137,7 +137,7 @@ func doSignCmd(cmd *cobra.Command, cpath string) {
 		if err != nil {
 			sylog.Fatalf("Failed to load key material: %v", err)
 		}
-		opts = append(opts, singularity.OptSignWithSigner(s))
+		opts = append(opts, sifsignature.OptSignWithSigner(s))
 
 	default:
 		sylog.Infof("Signing image with PGP key material")
@@ -150,21 +150,21 @@ func doSignCmd(cmd *cobra.Command, cpath string) {
 			f = selectEntityInteractive()
 		}
 		f = decryptSelectedEntityInteractive(f)
-		opts = append(opts, singularity.OptSignEntitySelector(f))
+		opts = append(opts, sifsignature.OptSignEntitySelector(f))
 	}
 
 	// Set group option, if applicable.
 	if cmd.Flag(signSifGroupIDFlag.Name).Changed || cmd.Flag(signOldSifGroupIDFlag.Name).Changed {
-		opts = append(opts, singularity.OptSignGroup(sifGroupID))
+		opts = append(opts, sifsignature.OptSignGroup(sifGroupID))
 	}
 
 	// Set object option, if applicable.
 	if cmd.Flag(signSifDescSifIDFlag.Name).Changed || cmd.Flag(signSifDescIDFlag.Name).Changed {
-		opts = append(opts, singularity.OptSignObjects(sifDescID))
+		opts = append(opts, sifsignature.OptSignObjects(sifDescID))
 	}
 
 	// Sign the image.
-	if err := singularity.Sign(cmd.Context(), cpath, opts...); err != nil {
+	if err := sifsignature.Sign(cmd.Context(), cpath, opts...); err != nil {
 		sylog.Fatalf("Failed to sign container: %v", err)
 	}
 	sylog.Infof("Signature created and applied to image '%v'", cpath)

cmd/internal/cli/verify.go
@@ -13,8 +13,8 @@ import (
 	"github.com/sigstore/sigstore/pkg/signature"
 	"github.com/spf13/cobra"
 	"github.com/sylabs/singularity/docs"
-	"github.com/sylabs/singularity/internal/app/singularity"
 	"github.com/sylabs/singularity/internal/pkg/remote/endpoint"
+	sifsignature "github.com/sylabs/singularity/internal/pkg/signature"
 	"github.com/sylabs/singularity/pkg/cmdline"
 	"github.com/sylabs/singularity/pkg/sylog"
 )
@@ -212,7 +212,7 @@ var VerifyCmd = &cobra.Command{
 }
 
 func doVerifyCmd(cmd *cobra.Command, cpath string) {
-	var opts []singularity.VerifyOpt
+	var opts []sifsignature.VerifyOpt
 
 	switch {
 	case cmd.Flag(verifyCertificateFlag.Name).Changed:
@@ -222,14 +222,14 @@ func doVerifyCmd(cmd *cobra.Command, cpath string) {
 		if err != nil {
 			sylog.Fatalf("Failed to load certificate: %v", err)
 		}
-		opts = append(opts, singularity.OptVerifyWithCertificate(c))
+		opts = append(opts, sifsignature.OptVerifyWithCertificate(c))
 
 		if cmd.Flag(verifyCertificateIntermediatesFlag.Name).Changed {
 			p, err := loadCertificatePool(certificateIntermediatesPath)
 			if err != nil {
 				sylog.Fatalf("Failed to load intermediate certificates: %v", err)
 			}
-			opts = append(opts, singularity.OptVerifyWithIntermediates(p))
+			opts = append(opts, sifsignature.OptVerifyWithIntermediates(p))
 		}
 
 		if cmd.Flag(verifyCertificateRootsFlag.Name).Changed {
@@ -237,11 +237,11 @@ func doVerifyCmd(cmd *cobra.Command, cpath string) {
 			if err != nil {
 				sylog.Fatalf("Failed to load root certificates: %v", err)
 			}
-			opts = append(opts, singularity.OptVerifyWithRoots(p))
+			opts = append(opts, sifsignature.OptVerifyWithRoots(p))
 		}
 
 		if cmd.Flag(verifyOCSPFlag.Name).Changed {
-			opts = append(opts, singularity.OptVerifyWithOCSP())
+			opts = append(opts, sifsignature.OptVerifyWithOCSP())
 		}
 
 	case cmd.Flag(verifyPublicKeyFlag.Name).Changed:
@@ -251,50 +251,50 @@ func doVerifyCmd(cmd *cobra.Command, cpath string) {
 		if err != nil {
 			sylog.Fatalf("Failed to load key material: %v", err)
 		}
-		opts = append(opts, singularity.OptVerifyWithVerifier(v))
+		opts = append(opts, sifsignature.OptVerifyWithVerifier(v))
 
 	default:
 		sylog.Infof("Verifying image with PGP key material")
 
 		// Set keyserver option, if applicable.
 		if localVerify {
-			opts = append(opts, singularity.OptVerifyWithPGP())
+			opts = append(opts, sifsignature.OptVerifyWithPGP())
 		} else {
 			co, err := getKeyserverClientOpts(keyServerURI, endpoint.KeyserverVerifyOp)
 			if err != nil {
 				sylog.Fatalf("Error while getting keyserver client config: %v", err)
 			}
-			opts = append(opts, singularity.OptVerifyWithPGP(co...))
+			opts = append(opts, sifsignature.OptVerifyWithPGP(co...))
 		}
 	}
 
 	// Set group option, if applicable.
 	if cmd.Flag(verifySifGroupIDFlag.Name).Changed || cmd.Flag(verifyOldSifGroupIDFlag.Name).Changed {
-		opts = append(opts, singularity.OptVerifyGroup(sifGroupID))
+		opts = append(opts, sifsignature.OptVerifyGroup(sifGroupID))
 	}
 
 	// Set object option, if applicable.
 	if cmd.Flag(verifySifDescSifIDFlag.Name).Changed || cmd.Flag(verifySifDescIDFlag.Name).Changed {
-		opts = append(opts, singularity.OptVerifyObject(sifDescID))
+		opts = append(opts, sifsignature.OptVerifyObject(sifDescID))
 	}
 
 	// Set all option, if applicable.
 	if verifyAll {
-		opts = append(opts, singularity.OptVerifyAll())
+		opts = append(opts, sifsignature.OptVerifyAll())
 	}
 
 	// Set legacy option, if applicable.
 	if verifyLegacy {
-		opts = append(opts, singularity.OptVerifyLegacy())
+		opts = append(opts, sifsignature.OptVerifyLegacy())
 	}
 
 	// Set callback option.
 	if jsonVerify {
 		var kl keyList
 
-		opts = append(opts, singularity.OptVerifyCallback(getJSONCallback(&kl)))
+		opts = append(opts, sifsignature.OptVerifyCallback(getJSONCallback(&kl)))
 
-		verifyErr := singularity.Verify(cmd.Context(), cpath, opts...)
+		verifyErr := sifsignature.Verify(cmd.Context(), cpath, opts...)
 
 		// Always output JSON.
 		if err := outputJSON(os.Stdout, kl); err != nil {
@@ -305,9 +305,9 @@ func doVerifyCmd(cmd *cobra.Command, cpath string) {
 			sylog.Fatalf("Failed to verify container: %v", verifyErr)
 		}
 	} else {
-		opts = append(opts, singularity.OptVerifyCallback(outputVerify))
+		opts = append(opts, sifsignature.OptVerifyCallback(outputVerify))
 
-		if err := singularity.Verify(cmd.Context(), cpath, opts...); err != nil {
+		if err := sifsignature.Verify(cmd.Context(), cpath, opts...); err != nil {
 			sylog.Fatalf("Failed to verify container: %v", err)
 		}
 

internal/app/singularity/push.go
@@ -18,6 +18,7 @@ import (
 	keyclient "github.com/sylabs/scs-key-client/client"
 	"github.com/sylabs/scs-library-client/client"
 	"github.com/sylabs/sif/v2/pkg/sif"
+	"github.com/sylabs/singularity/internal/pkg/signature"
 	"github.com/sylabs/singularity/internal/pkg/util/fs"
 	"github.com/sylabs/singularity/pkg/sylog"
 	"github.com/vbauerster/mpb/v8"
@@ -96,7 +97,7 @@ func LibraryPush(ctx context.Context, pushSpec LibraryPushSpec, libraryConfig *c
 
 	if !pushSpec.AllowUnsigned {
 		// Check if the container has a valid signature.
-		if err := Verify(ctx, pushSpec.SourceFile, OptVerifyWithPGP(co...)); err != nil {
+		if err := signature.Verify(ctx, pushSpec.SourceFile, signature.OptVerifyWithPGP(co...)); err != nil {
 			sylog.Warningf("%v", err)
 			return ErrLibraryUnsigned
 		}

internal/pkg/client/library/pull.go
@@ -16,9 +16,9 @@ import (
 	keyclient "github.com/sylabs/scs-key-client/client"
 	libclient "github.com/sylabs/scs-library-client/client"
 	scslibrary "github.com/sylabs/scs-library-client/client"
-	"github.com/sylabs/singularity/internal/app/singularity"
 	"github.com/sylabs/singularity/internal/pkg/cache"
 	"github.com/sylabs/singularity/internal/pkg/client"
+	"github.com/sylabs/singularity/internal/pkg/signature"
 	"github.com/sylabs/singularity/internal/pkg/util/fs"
 	"github.com/sylabs/singularity/pkg/sylog"
 	"golang.org/x/term"
@@ -140,7 +140,7 @@ func PullToFile(ctx context.Context, imgCache *cache.Handle, pullTo string, pull
 		}
 	}
 
-	if err := singularity.Verify(ctx, pullTo, singularity.OptVerifyWithPGP(co...)); err != nil {
+	if err := signature.Verify(ctx, pullTo, signature.OptVerifyWithPGP(co...)); err != nil {
 		sylog.Warningf("%v", err)
 		return pullTo, ErrLibraryPullUnsigned
 	}

apptainer/apptainer

Signed-off-by: Edita Kizinevic <edita.kizinevic@cern.ch>

cmd/internal/cli/pgp.go
@@ -22,7 +22,7 @@ import (
 
 	"github.com/ProtonMail/go-crypto/openpgp"
 	"github.com/ProtonMail/go-crypto/openpgp/packet"
-	"github.com/apptainer/apptainer/internal/app/apptainer"
+	sifsignature "github.com/apptainer/apptainer/internal/pkg/signature"
 	"github.com/apptainer/apptainer/internal/pkg/util/interactive"
 	"github.com/apptainer/apptainer/pkg/sylog"
 	"github.com/apptainer/apptainer/pkg/sypgp"
@@ -224,8 +224,8 @@ type keyList struct {
 	SignerKeys []*key
 }
 
-// getJSONCallback returns an apptainer.VerifyCallback that appends to kl.
-func getJSONCallback(kl *keyList) apptainer.VerifyCallback {
+// getJSONCallback returns a signature.VerifyCallback that appends to kl.
+func getJSONCallback(kl *keyList) sifsignature.VerifyCallback {
 	return func(f *sif.FileImage, r integrity.VerifyResult) bool {
 		name, fp := "unknown", ""
 		var keyLocal, keyCheck bool

cmd/internal/cli/sign.go
@@ -13,7 +13,7 @@ import (
 	"crypto"
 
 	"github.com/apptainer/apptainer/docs"
-	"github.com/apptainer/apptainer/internal/app/apptainer"
+	sifsignature "github.com/apptainer/apptainer/internal/pkg/signature"
 	"github.com/apptainer/apptainer/pkg/cmdline"
 	"github.com/apptainer/apptainer/pkg/sylog"
 	"github.com/apptainer/apptainer/pkg/sypgp"
@@ -130,7 +130,7 @@ var SignCmd = &cobra.Command{
 }
 
 func doSignCmd(cmd *cobra.Command, cpath string) {
-	var opts []apptainer.SignOpt
+	var opts []sifsignature.SignOpt
 
 	// Set key material.
 	switch {
@@ -141,7 +141,7 @@ func doSignCmd(cmd *cobra.Command, cpath string) {
 		if err != nil {
 			sylog.Fatalf("Failed to load key material: %v", err)
 		}
-		opts = append(opts, apptainer.OptSignWithSigner(s))
+		opts = append(opts, sifsignature.OptSignWithSigner(s))
 
 	default:
 		sylog.Infof("Signing image with PGP key material")
@@ -154,21 +154,21 @@ func doSignCmd(cmd *cobra.Command, cpath string) {
 			f = selectEntityInteractive()
 		}
 		f = decryptSelectedEntityInteractive(f)
-		opts = append(opts, apptainer.OptSignEntitySelector(f))
+		opts = append(opts, sifsignature.OptSignEntitySelector(f))
 	}
 
 	// Set group option, if applicable.
 	if cmd.Flag(signSifGroupIDFlag.Name).Changed || cmd.Flag(signOldSifGroupIDFlag.Name).Changed {
-		opts = append(opts, apptainer.OptSignGroup(sifGroupID))
+		opts = append(opts, sifsignature.OptSignGroup(sifGroupID))
 	}
 
 	// Set object option, if applicable.
 	if cmd.Flag(signSifDescSifIDFlag.Name).Changed || cmd.Flag(signSifDescIDFlag.Name).Changed {
-		opts = append(opts, apptainer.OptSignObjects(sifDescID))
+		opts = append(opts, sifsignature.OptSignObjects(sifDescID))
 	}
 
 	// Sign the image.
-	if err := apptainer.Sign(cmd.Context(), cpath, opts...); err != nil {
+	if err := sifsignature.Sign(cmd.Context(), cpath, opts...); err != nil {
 		sylog.Fatalf("Failed to sign container: %v", err)
 	}
 	sylog.Infof("Signature created and applied to image '%v'", cpath)

cmd/internal/cli/verify.go
@@ -15,8 +15,8 @@ import (
 	"os"
 
 	"github.com/apptainer/apptainer/docs"
-	"github.com/apptainer/apptainer/internal/app/apptainer"
 	"github.com/apptainer/apptainer/internal/pkg/remote/endpoint"
+	sifsignature "github.com/apptainer/apptainer/internal/pkg/signature"
 	"github.com/apptainer/apptainer/pkg/cmdline"
 	"github.com/apptainer/apptainer/pkg/sylog"
 	"github.com/sigstore/sigstore/pkg/signature"
@@ -216,7 +216,7 @@ var VerifyCmd = &cobra.Command{
 }
 
 func doVerifyCmd(cmd *cobra.Command, cpath string) {
-	var opts []apptainer.VerifyOpt
+	var opts []sifsignature.VerifyOpt
 
 	switch {
 	case cmd.Flag(verifyCertificateFlag.Name).Changed:
@@ -226,14 +226,14 @@ func doVerifyCmd(cmd *cobra.Command, cpath string) {
 		if err != nil {
 			sylog.Fatalf("Failed to load certificate: %v", err)
 		}
-		opts = append(opts, apptainer.OptVerifyWithCertificate(c))
+		opts = append(opts, sifsignature.OptVerifyWithCertificate(c))
 
 		if cmd.Flag(verifyCertificateIntermediatesFlag.Name).Changed {
 			p, err := loadCertificatePool(certificateIntermediatesPath)
 			if err != nil {
 				sylog.Fatalf("Failed to load intermediate certificates: %v", err)
 			}
-			opts = append(opts, apptainer.OptVerifyWithIntermediates(p))
+			opts = append(opts, sifsignature.OptVerifyWithIntermediates(p))
 		}
 
 		if cmd.Flag(verifyCertificateRootsFlag.Name).Changed {
@@ -241,11 +241,11 @@ func doVerifyCmd(cmd *cobra.Command, cpath string) {
 			if err != nil {
 				sylog.Fatalf("Failed to load root certificates: %v", err)
 			}
-			opts = append(opts, apptainer.OptVerifyWithRoots(p))
+			opts = append(opts, sifsignature.OptVerifyWithRoots(p))
 		}
 
 		if cmd.Flag(verifyOCSPFlag.Name).Changed {
-			opts = append(opts, apptainer.OptVerifyWithOCSP())
+			opts = append(opts, sifsignature.OptVerifyWithOCSP())
 		}
 
 	case cmd.Flag(verifyPublicKeyFlag.Name).Changed:
@@ -255,50 +255,50 @@ func doVerifyCmd(cmd *cobra.Command, cpath string) {
 		if err != nil {
 			sylog.Fatalf("Failed to load key material: %v", err)
 		}
-		opts = append(opts, apptainer.OptVerifyWithVerifier(v))
+		opts = append(opts, sifsignature.OptVerifyWithVerifier(v))
 
 	default:
 		sylog.Infof("Verifying image with PGP key material")
 
 		// Set keyserver option, if applicable.
 		if localVerify {
-			opts = append(opts, apptainer.OptVerifyWithPGP())
+			opts = append(opts, sifsignature.OptVerifyWithPGP())
 		} else {
 			co, err := getKeyserverClientOpts(keyServerURI, endpoint.KeyserverVerifyOp)
 			if err != nil {
 				sylog.Fatalf("Error while getting keyserver client config: %v", err)
 			}
-			opts = append(opts, apptainer.OptVerifyWithPGP(co...))
+			opts = append(opts, sifsignature.OptVerifyWithPGP(co...))
 		}
 	}
 
 	// Set group option, if applicable.
 	if cmd.Flag(verifySifGroupIDFlag.Name).Changed || cmd.Flag(verifyOldSifGroupIDFlag.Name).Changed {
-		opts = append(opts, apptainer.OptVerifyGroup(sifGroupID))
+		opts = append(opts, sifsignature.OptVerifyGroup(sifGroupID))
 	}
 
 	// Set object option, if applicable.
 	if cmd.Flag(verifySifDescSifIDFlag.Name).Changed || cmd.Flag(verifySifDescIDFlag.Name).Changed {
-		opts = append(opts, apptainer.OptVerifyObject(sifDescID))
+		opts = append(opts, sifsignature.OptVerifyObject(sifDescID))
 	}
 
 	// Set all option, if applicable.
 	if verifyAll {
-		opts = append(opts, apptainer.OptVerifyAll())
+		opts = append(opts, sifsignature.OptVerifyAll())
 	}
 
 	// Set legacy option, if applicable.
 	if verifyLegacy {
-		opts = append(opts, apptainer.OptVerifyLegacy())
+		opts = append(opts, sifsignature.OptVerifyLegacy())
 	}
 
 	// Set callback option.
 	if jsonVerify {
 		var kl keyList
 
-		opts = append(opts, apptainer.OptVerifyCallback(getJSONCallback(&kl)))
+		opts = append(opts, sifsignature.OptVerifyCallback(getJSONCallback(&kl)))
 
-		verifyErr := apptainer.Verify(cmd.Context(), cpath, opts...)
+		verifyErr := sifsignature.Verify(cmd.Context(), cpath, opts...)
 
 		// Always output JSON.
 		if err := outputJSON(os.Stdout, kl); err != nil {
@@ -309,9 +309,9 @@ func doVerifyCmd(cmd *cobra.Command, cpath string) {
 			sylog.Fatalf("Failed to verify container: %v", verifyErr)
 		}
 	} else {
-		opts = append(opts, apptainer.OptVerifyCallback(outputVerify))
+		opts = append(opts, sifsignature.OptVerifyCallback(outputVerify))
 
-		if err := apptainer.Verify(cmd.Context(), cpath, opts...); err != nil {
+		if err := sifsignature.Verify(cmd.Context(), cpath, opts...); err != nil {
 			sylog.Fatalf("Failed to verify container: %v", err)
 		}
 

internal/app/apptainer/push.go
@@ -19,6 +19,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/apptainer/apptainer/internal/pkg/signature"
 	"github.com/apptainer/apptainer/internal/pkg/util/fs"
 	"github.com/apptainer/apptainer/pkg/sylog"
 	keyClient "github.com/apptainer/container-key-client/client"
@@ -100,7 +101,7 @@ func LibraryPush(ctx context.Context, pushSpec LibraryPushSpec, libraryConfig *c
 
 	if !pushSpec.AllowUnsigned {
 		// Check if the container has a valid signature.
-		if err := Verify(ctx, pushSpec.SourceFile, OptVerifyWithPGP(co...)); err != nil {
+		if err := signature.Verify(ctx, pushSpec.SourceFile, signature.OptVerifyWithPGP(co...)); err != nil {
 			sylog.Warningf("%v", err)
 			return ErrLibraryUnsigned
 		}

internal/pkg/client/library/pull.go
@@ -17,9 +17,9 @@ import (
 	"os"
 	"time"
 
-	"github.com/apptainer/apptainer/internal/app/apptainer"
 	"github.com/apptainer/apptainer/internal/pkg/cache"
 	"github.com/apptainer/apptainer/internal/pkg/client"
+	"github.com/apptainer/apptainer/internal/pkg/signature"
 	"github.com/apptainer/apptainer/internal/pkg/util/fs"
 	"github.com/apptainer/apptainer/pkg/sylog"
 	keyClient "github.com/apptainer/container-key-client/client"
@@ -143,7 +143,7 @@ func PullToFile(ctx context.Context, imgCache *cache.Handle, pullTo string, pull
 		}
 	}
 
-	if err := apptainer.Verify(ctx, pullTo, apptainer.OptVerifyWithPGP(co...)); err != nil {
+	if err := signature.Verify(ctx, pullTo, signature.OptVerifyWithPGP(co...)); err != nil {
 		sylog.Warningf("%v", err)
 		return pullTo, ErrLibraryPullUnsigned
 	}