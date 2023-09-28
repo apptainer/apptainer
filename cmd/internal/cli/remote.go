sylabs/singularity

chore: refactor library token/identity to i/p/c/library
Move the code that calls the library service to retrieve identity /
token information, and remove duplication.

Since this code operates against the library, it makes sense to have it
in intenal/pkg/client/library

We are also going to need it accessible there for OCI-SIF library://
push/pull.

cmd/internal/cli/remote.go
@@ -15,6 +15,7 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/sylabs/singularity/docs"
 	"github.com/sylabs/singularity/internal/app/singularity"
+	"github.com/sylabs/singularity/internal/pkg/client/library"
 	"github.com/sylabs/singularity/internal/pkg/remote"
 	"github.com/sylabs/singularity/pkg/cmdline"
 	"github.com/sylabs/singularity/pkg/syfs"
@@ -228,7 +229,7 @@ var RemoteGetLoginPasswordCmd = &cobra.Command{
 			sylog.Errorf("Error initializing config: %v", err)
 		}
 
-		password, err := singularity.RemoteGetLoginPassword(config)
+		password, err := library.GetOCIToken(config)
 		if err != nil {
 			sylog.Errorf("error: %v", err)
 		}

internal/app/singularity/remote_status.go
@@ -1,5 +1,5 @@
 // Copyright (c) 2020, Control Command Inc. All rights reserved.
-// Copyright (c) 2019-2022, Sylabs Inc. All rights reserved.
+// Copyright (c) 2019-2023, Sylabs Inc. All rights reserved.
 // This software is licensed under a 3-clause BSD license. Please consult the
 // LICENSE.md file distributed with the sources of this project regarding your
 // rights to use or distribute this software.
@@ -7,14 +7,13 @@
 package singularity
 
 import (
-	"encoding/json"
 	"fmt"
-	"net/http"
 	"os"
 	"sort"
 	"text/tabwriter"
 
 	scslibclient "github.com/sylabs/scs-library-client/client"
+	"github.com/sylabs/singularity/internal/pkg/client/library"
 	"github.com/sylabs/singularity/internal/pkg/remote"
 	"github.com/sylabs/singularity/internal/pkg/remote/endpoint"
 	"github.com/sylabs/singularity/pkg/sylog"
@@ -120,9 +119,8 @@ func RemoteStatus(usrConfigFile, name string) (err error) {
 	if err != nil {
 		return fmt.Errorf("could not get library client configuration: %v", err)
 	}
-	if err := printLoggedInIdentity(libClientConfig); err != nil {
-		return err
-	}
+
+	printLoggedInIdentity(libClientConfig)
 
 	return doTokenCheck(e)
 }
@@ -139,45 +137,12 @@ func doStatusCheck(name string, sp endpoint.Service) *status {
 	return &status{name: name, uri: uri, status: "OK", version: version}
 }
 
-func printLoggedInIdentity(config *scslibclient.Config) error {
-	path := userServicePath
-	endPoint := config.BaseURL + path
+func printLoggedInIdentity(config *scslibclient.Config) {
+	username, email, err := library.GetIdentity(config)
 
-	req, err := http.NewRequest(http.MethodGet, endPoint, nil)
-	if err != nil {
-		return fmt.Errorf("error creating new request: %v", err)
+	if err == nil && len(username) > 0 {
+		fmt.Printf("\nLogged in as: %s <%s>\n\n", username, email)
 	}
-	req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", config.AuthToken))
-	res, err := config.HTTPClient.Do(req)
-	if err != nil {
-		return fmt.Errorf("error making request: %v", err)
-	}
-	defer res.Body.Close()
-
-	if res.StatusCode != http.StatusOK {
-		sylog.Debugf("Status Code: %v", res.StatusCode)
-		if res.StatusCode == http.StatusUnauthorized {
-			// This simply means we are not logged in, which in this context is not an error condition.
-			return nil
-		} else if res.StatusCode == http.StatusNotFound {
-			sylog.Warningf("endpoint for retrieving logged-in user's username/email is not available on the current remote library")
-			return nil
-		} else {
-			return fmt.Errorf("encountered error while trying to retrieve logged-in user's username/email: %v", res.StatusCode)
-		}
-	}
-
-	var ud userData
-	if err := json.NewDecoder(res.Body).Decode(&ud); err != nil {
-		return fmt.Errorf("error decoding json response: %v", err)
-	}
-
-	if len(ud.Username) > 0 {
-		// TODO: Right now, it doesn't seem like the realname field ever contains anything different than the username field, so it would be redundant to print them both. If this ever changes, we can add another %s to the format string and output ud.Realname, as well. (It's already in the struct.)
-		fmt.Printf("\nLogged in as: %s <%s>\n\n", ud.Username, ud.Email)
-	}
-
-	return nil
 }
 
 func doTokenCheck(e *endpoint.Config) error {

internal/pkg/client/library/user.go
@@ -1,0 +1,87 @@
+// Copyright (c) 2023, Sylabs Inc. All rights reserved.
+// This software is licensed under a 3-clause BSD license. Please consult the
+// LICENSE.md file distributed with the sources of this project regarding your
+// rights to use or distribute this software.
+package library
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	scslibrary "github.com/sylabs/scs-library-client/client"
+	"github.com/sylabs/singularity/pkg/sylog"
+)
+
+const (
+	userServicePath = "/v1/rbac/users/current"
+)
+
+type userOidcMeta struct {
+	Secret string `json:"secret"`
+}
+
+type userData struct {
+	OidcMeta userOidcMeta `json:"oidc_user_meta"`
+	Email    string       `json:"email"`
+	Realname string       `json:"realname"`
+	Username string       `json:"username"`
+}
+
+// getUserData retrives auth service user information from the current remote.
+func getUserData(config *scslibrary.Config) (*userData, error) {
+	client := http.Client{Timeout: 5 * time.Second}
+	path := userServicePath
+	endPoint := config.BaseURL + path
+
+	req, err := http.NewRequest(http.MethodGet, endPoint, nil)
+	if err != nil {
+		return nil, fmt.Errorf("error creating new request: %v", err)
+	}
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", config.AuthToken))
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("error making request: %v", err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		sylog.Debugf("Status Code: %v", res.StatusCode)
+		if res.StatusCode == http.StatusUnauthorized {
+			return nil, fmt.Errorf("must be logged in to retrieve user data")
+		}
+		if res.StatusCode == http.StatusNotFound {
+			return nil, fmt.Errorf("user endpoint not found")
+		}
+		return nil, fmt.Errorf("status is not ok: %v", res.StatusCode)
+	}
+
+	var ud userData
+	err = json.NewDecoder(res.Body).Decode(&ud)
+	if err != nil {
+		return nil, fmt.Errorf("error decoding json response: %v", err)
+	}
+	return &ud, nil
+}
+
+// GetIdentity returns the username and email for the logged in user.
+func GetIdentity(config *scslibrary.Config) (user, email string, err error) {
+	ud, err := getUserData(config)
+	if err != nil {
+		return "", "", err
+	}
+	return ud.Username, ud.Email, nil
+}
+
+// GetOCIToken retrieves the OCI registry token for the logged in user.
+func GetOCIToken(config *scslibrary.Config) (string, error) {
+	ud, err := getUserData(config)
+	if err != nil {
+		return "", err
+	}
+	if ud.OidcMeta.Secret == "" {
+		return "", fmt.Errorf("no token was received from service")
+	}
+	return ud.OidcMeta.Secret, nil
+}

apptainer/apptainer

Signed-off-by: Dave Dykstra <2129743+DrDaveD@users.noreply.github.com>

cmd/internal/cli/remote.go
@@ -18,6 +18,7 @@ import (
 
 	"github.com/apptainer/apptainer/docs"
 	"github.com/apptainer/apptainer/internal/app/apptainer"
+	"github.com/apptainer/apptainer/internal/pkg/client/library"
 	"github.com/apptainer/apptainer/internal/pkg/remote"
 	"github.com/apptainer/apptainer/pkg/cmdline"
 	"github.com/apptainer/apptainer/pkg/syfs"
@@ -249,7 +250,7 @@ var RemoteGetLoginPasswordCmd = &cobra.Command{
 			sylog.Errorf("Error initializing config: %v", err)
 		}
 
-		password, err := apptainer.RemoteGetLoginPassword(config)
+		password, err := library.GetOCIToken(config)
 		if err != nil {
 			sylog.Errorf("error: %v", err)
 		}

internal/app/apptainer/remote_status.go
@@ -3,7 +3,7 @@
 //   For website terms of use, trademark policy, privacy policy and other
 //   project policies see https://lfprojects.org/policies
 // Copyright (c) 2020, Control Command Inc. All rights reserved.
-// Copyright (c) 2019-2022, Sylabs Inc. All rights reserved.
+// Copyright (c) 2019-2023, Sylabs Inc. All rights reserved.
 // This software is licensed under a 3-clause BSD license. Please consult the
 // LICENSE.md file distributed with the sources of this project regarding your
 // rights to use or distribute this software.
@@ -11,17 +11,16 @@
 package apptainer
 
 import (
-	"encoding/json"
 	"fmt"
-	"net/http"
 	"os"
 	"sort"
 	"text/tabwriter"
 
+	"github.com/apptainer/apptainer/internal/pkg/client/library"
 	"github.com/apptainer/apptainer/internal/pkg/remote"
 	"github.com/apptainer/apptainer/internal/pkg/remote/endpoint"
 	"github.com/apptainer/apptainer/pkg/sylog"
-	scslibclient "github.com/apptainer/container-library-client/client"
+	containerclient "github.com/apptainer/container-library-client/client"
 	"golang.org/x/text/cases"
 	"golang.org/x/text/language"
 )
@@ -124,9 +123,8 @@ func RemoteStatus(usrConfigFile, name string) (err error) {
 	if err != nil {
 		return fmt.Errorf("could not get library client configuration: %v", err)
 	}
-	if err := printLoggedInIdentity(libClientConfig); err != nil {
-		return err
-	}
+
+	printLoggedInIdentity(libClientConfig)
 
 	return doTokenCheck(e)
 }
@@ -143,45 +141,12 @@ func doStatusCheck(name string, sp endpoint.Service) *status {
 	return &status{name: name, uri: uri, status: "OK", version: version}
 }
 
-func printLoggedInIdentity(config *scslibclient.Config) error {
-	path := userServicePath
-	endPoint := config.BaseURL + path
+func printLoggedInIdentity(config *containerclient.Config) {
+	username, email, err := library.GetIdentity(config)
 
-	req, err := http.NewRequest(http.MethodGet, endPoint, nil)
-	if err != nil {
-		return fmt.Errorf("error creating new request: %v", err)
+	if err == nil && len(username) > 0 {
+		fmt.Printf("\nLogged in as: %s <%s>\n\n", username, email)
 	}
-	req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", config.AuthToken))
-	res, err := config.HTTPClient.Do(req)
-	if err != nil {
-		return fmt.Errorf("error making request: %v", err)
-	}
-	defer res.Body.Close()
-
-	if res.StatusCode != http.StatusOK {
-		sylog.Debugf("Status Code: %v", res.StatusCode)
-		if res.StatusCode == http.StatusUnauthorized {
-			// This simply means we are not logged in, which in this context is not an error condition.
-			return nil
-		} else if res.StatusCode == http.StatusNotFound {
-			sylog.Warningf("endpoint for retrieving logged-in user's username/email is not available on the current remote library")
-			return nil
-		} else {
-			return fmt.Errorf("encountered error while trying to retrieve logged-in user's username/email: %v", res.StatusCode)
-		}
-	}
-
-	var ud userData
-	if err := json.NewDecoder(res.Body).Decode(&ud); err != nil {
-		return fmt.Errorf("error decoding json response: %v", err)
-	}
-
-	if len(ud.Username) > 0 {
-		// TODO: Right now, it doesn't seem like the realname field ever contains anything different than the username field, so it would be redundant to print them both. If this ever changes, we can add another %s to the format string and output ud.Realname, as well. (It's already in the struct.)
-		fmt.Printf("\nLogged in as: %s <%s>\n\n", ud.Username, ud.Email)
-	}
-
-	return nil
 }
 
 func doTokenCheck(e *endpoint.Config) error {

internal/pkg/client/library/user.go
@@ -1,0 +1,92 @@
+// Copyright (c) Contributors to the Apptainer project, established as
+//   Apptainer a Series of LF Projects LLC.
+//   For website terms of use, trademark policy, privacy policy and other
+//   project policies see https://lfprojects.org/policies
+// Copyright (c) 2023, Sylabs Inc. All rights reserved.
+// This software is licensed under a 3-clause BSD license. Please consult the
+// LICENSE.md file distributed with the sources of this project regarding your
+// rights to use or distribute this software.
+
+package library
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/apptainer/apptainer/pkg/sylog"
+	containerclient "github.com/apptainer/container-library-client/client"
+)
+
+const (
+	userServicePath = "/v1/rbac/users/current"
+)
+
+type userOidcMeta struct {
+	Secret string `json:"secret"`
+}
+
+type userData struct {
+	OidcMeta userOidcMeta `json:"oidc_user_meta"`
+	Email    string       `json:"email"`
+	Realname string       `json:"realname"`
+	Username string       `json:"username"`
+}
+
+// getUserData retrives auth service user information from the current remote.
+func getUserData(config *containerclient.Config) (*userData, error) {
+	client := http.Client{Timeout: 5 * time.Second}
+	path := userServicePath
+	endPoint := config.BaseURL + path
+
+	req, err := http.NewRequest(http.MethodGet, endPoint, nil)
+	if err != nil {
+		return nil, fmt.Errorf("error creating new request: %v", err)
+	}
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", config.AuthToken))
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("error making request: %v", err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		sylog.Debugf("Status Code: %v", res.StatusCode)
+		if res.StatusCode == http.StatusUnauthorized {
+			return nil, fmt.Errorf("must be logged in to retrieve user data")
+		}
+		if res.StatusCode == http.StatusNotFound {
+			return nil, fmt.Errorf("user endpoint not found")
+		}
+		return nil, fmt.Errorf("status is not ok: %v", res.StatusCode)
+	}
+
+	var ud userData
+	err = json.NewDecoder(res.Body).Decode(&ud)
+	if err != nil {
+		return nil, fmt.Errorf("error decoding json response: %v", err)
+	}
+	return &ud, nil
+}
+
+// GetIdentity returns the username and email for the logged in user.
+func GetIdentity(config *containerclient.Config) (user, email string, err error) {
+	ud, err := getUserData(config)
+	if err != nil {
+		return "", "", err
+	}
+	return ud.Username, ud.Email, nil
+}
+
+// GetOCIToken retrieves the OCI registry token for the logged in user.
+func GetOCIToken(config *containerclient.Config) (string, error) {
+	ud, err := getUserData(config)
+	if err != nil {
+		return "", err
+	}
+	if ud.OidcMeta.Secret == "" {
+		return "", fmt.Errorf("no token was received from service")
+	}
+	return ud.OidcMeta.Secret, nil
+}