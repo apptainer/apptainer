apptainer/apptainer

commit 4a0d2b81b1c2784d7df196575dfffb47478532a7

Lookup and store user/group information in stage one prior to creating namespaces
Signed-off-by: Cédric Clerget <cedric.clerget@gmail.com>

internal/pkg/runtime/engine/apptainer/prepare_linux.go
@@ -131,11 +131,13 @@ func (e *EngineOperations) PrepareConfig(starterConfig *starter.Config) error {
 
 	uid := e.EngineConfig.GetTargetUID()
 	gids := e.EngineConfig.GetTargetGID()
+	useTargetIDs := false
 
 	if os.Getuid() == 0 && (uid != 0 || len(gids) > 0) {
 		starterConfig.SetTargetUID(uid)
 		starterConfig.SetTargetGID(gids)
 		e.EngineConfig.OciConfig.SetProcessNoNewPrivileges(true)
+		useTargetIDs = true
 	}
 
 	userNS := !starterConfig.GetIsSUID() || e.EngineConfig.GetFakeroot()
@@ -147,6 +149,8 @@ func (e *EngineOperations) PrepareConfig(starterConfig *starter.Config) error {
 			return err
 		}
 	} else {
+		e.setUserInfo(useTargetIDs)
+
 		if err := e.prepareContainerConfig(starterConfig); err != nil {
 			return err
 		}
@@ -1485,4 +1489,52 @@ func (e *EngineOperations) loadImage(path string, writable bool, userNS bool) (*
 	}
 
 	return imgObject, imgErr
+}
+
+func (e *EngineOperations) setUserInfo(useTargetIDs bool) {
+	var gids []int
+
+	pw, err := user.Current()
+	if err != nil {
+		return
+	}
+
+	e.EngineConfig.JSON.UserInfo.Home = pw.Dir
+
+	if useTargetIDs {
+		pw, err = user.GetPwUID(uint32(e.EngineConfig.GetTargetUID()))
+		if err == nil {
+			e.EngineConfig.JSON.UserInfo.Username = pw.Name
+			e.EngineConfig.JSON.UserInfo.Gecos = pw.Gecos
+			e.EngineConfig.JSON.UserInfo.UID = int(pw.UID)
+			e.EngineConfig.JSON.UserInfo.GID = int(pw.GID)
+		}
+	} else {
+		e.EngineConfig.JSON.UserInfo.Username = pw.Name
+		e.EngineConfig.JSON.UserInfo.Gecos = pw.Gecos
+		e.EngineConfig.JSON.UserInfo.UID = int(pw.UID)
+		e.EngineConfig.JSON.UserInfo.GID = int(pw.GID)
+	}
+
+	e.EngineConfig.JSON.UserInfo.Groups = make(map[int]string)
+
+	if useTargetIDs {
+		gids = e.EngineConfig.GetTargetGID()
+	} else {
+		gids, err = os.Getgroups()
+		if err != nil {
+			return
+		}
+	}
+
+	if pw != nil {
+		gids = append(gids, int(pw.GID))
+	}
+
+	for _, gid := range gids {
+		group, err := user.GetGrGID(uint32(gid))
+		if err == nil {
+			e.EngineConfig.JSON.UserInfo.Groups[gid] = group.Name
+		}
+	}
 }

internal/pkg/runtime/engine/apptainer/container_linux.go
@@ -14,6 +14,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	osuser "os/user"
 	"path/filepath"
 	"strconv"
 	"strings"
@@ -1695,9 +1696,8 @@ func (c *container) getHomePaths() (source string, dest string, err error) {
 		dest = filepath.Clean(c.engine.EngineConfig.GetHomeDest())
 		source, err = filepath.Abs(filepath.Clean(c.engine.EngineConfig.GetHomeSource()))
 	} else {
-		pw, err := user.CurrentOriginal()
-		if err == nil {
-			source = pw.Dir
+		source = c.engine.EngineConfig.JSON.UserInfo.Home
+		if source != "" {
 			if c.engine.EngineConfig.GetFakeroot() || os.Getuid() == 0 {
 				// Mount user home directory onto /root for
 				//  any root-mapped namespace
@@ -2285,7 +2285,7 @@ func (c *container) addIdentityMount(system *mount.System) error {
 		if err != nil {
 			sylog.Warningf("%s", err)
 		} else {
-			content, err := files.Passwd(passwd, home, uid)
+			content, err := files.Passwd(passwd, home, uid, c)
 			if err != nil {
 				sylog.Warningf("%s", err)
 			} else {
@@ -2308,7 +2308,7 @@ func (c *container) addIdentityMount(system *mount.System) error {
 
 	if c.engine.EngineConfig.File.ConfigGroup {
 		group := filepath.Join(rootfs, "/etc/group")
-		content, err := files.Group(group, uid, c.engine.EngineConfig.GetTargetGID())
+		content, err := files.Group(group, uid, c.engine.EngineConfig.GetTargetGID(), c)
 		if err != nil {
 			sylog.Warningf("%s", err)
 		} else {
@@ -2841,4 +2841,37 @@ func gocryptfsMount(params *image.MountParams, mfunc image.MountFunc) error {
 		return fmt.Errorf("could not locate the decrypted squashfs file, previous gocryptfs mount failed")
 	}
 	return imageDriver.Mount(params, mfunc)
+}
+
+func (c *container) GetPwUID(uid uint32) (*user.User, error) {
+	if c.engine.EngineConfig.JSON.UserInfo.Username == "" {
+		return nil, osuser.UnknownUserIdError(uid)
+	}
+	return &user.User{
+		Name:  c.engine.EngineConfig.JSON.UserInfo.Username,
+		UID:   uint32(c.engine.EngineConfig.JSON.UserInfo.UID),
+		GID:   uint32(c.engine.EngineConfig.JSON.UserInfo.GID),
+		Gecos: c.engine.EngineConfig.JSON.UserInfo.Gecos,
+		Dir:   c.engine.EngineConfig.JSON.UserInfo.Home,
+		Shell: c.engine.EngineConfig.JSON.UserInfo.Shell,
+	}, nil
+}
+
+func (c *container) GetGrGID(gid uint32) (*user.Group, error) {
+	name, ok := c.engine.EngineConfig.JSON.UserInfo.Groups[int(gid)]
+	if ok {
+		return &user.Group{
+			Name: name,
+			GID:  gid,
+		}, nil
+	}
+	return nil, osuser.UnknownGroupIdError(fmt.Sprintf("%d", gid))
+}
+
+func (c *container) Getgroups() ([]int, error) {
+	gids := make([]int, 0, len(c.engine.EngineConfig.JSON.UserInfo.Groups))
+	for gid := range c.engine.EngineConfig.JSON.UserInfo.Groups {
+		gids = append(gids, gid)
+	}
+	return gids, nil
 }

internal/pkg/util/fs/files/files_linux_test.go
@@ -24,11 +24,11 @@ func TestGroup(t *testing.T) {
 	var gids []int
 	uid := os.Getuid()
 
-	_, err := Group("/fake", uid, gids)
+	_, err := Group("/fake", uid, gids, nil)
 	if err == nil {
 		t.Errorf("should have failed with bad group file")
 	}
-	_, err = Group("/etc/group", uid, gids)
+	_, err = Group("/etc/group", uid, gids, nil)
 	if err != nil {
 		t.Errorf("should have passed with correct group file")
 	}
@@ -41,7 +41,7 @@ func TestGroup(t *testing.T) {
 	defer os.Remove(emptyGroup)
 	f.Close()
 
-	_, err = Group(emptyGroup, uid, gids)
+	_, err = Group(emptyGroup, uid, gids, nil)
 	if err != nil {
 		t.Error(err)
 	}
@@ -53,11 +53,11 @@ func TestPasswd(t *testing.T) {
 
 	uid := os.Getuid()
 
-	_, err := Passwd("/fake", "/fake", uid)
+	_, err := Passwd("/fake", "/fake", uid, nil)
 	if err == nil {
 		t.Errorf("should have failed with bad passwd file")
 	}
-	_, err = Passwd("/etc/passwd", "/home", uid)
+	_, err = Passwd("/etc/passwd", "/home", uid, nil)
 	if err != nil {
 		t.Errorf("should have passed with correct passwd file")
 	}
@@ -70,7 +70,7 @@ func TestPasswd(t *testing.T) {
 	defer os.Remove(emptyPasswd)
 	f.Close()
 
-	_, err = Passwd(emptyPasswd, "/home", uid)
+	_, err = Passwd(emptyPasswd, "/home", uid, nil)
 	if err != nil {
 		t.Error(err)
 	}

internal/pkg/util/fs/files/group.go
@@ -21,7 +21,7 @@ import (
 
 // Group creates a group template based on content of file provided in path,
 // updates content with current user information and returns content
-func Group(path string, uid int, gids []int) (content []byte, err error) {
+func Group(path string, uid int, gids []int, customLookup UserGroupLookup) (content []byte, err error) {
 	duplicate := false
 	var groups []int
 
@@ -37,16 +37,26 @@ func Group(path string, uid int, gids []int) (content []byte, err error) {
 	}
 	defer groupFile.Close()
 
-	pwInfo, err := user.GetPwUID(uint32(uid))
+	getPwUID := user.GetPwUID
+	getGrGID := user.GetGrGID
+	getGroups := os.Getgroups
+
+	if customLookup != nil {
+		getPwUID = customLookup.GetPwUID
+		getGrGID = customLookup.GetGrGID
+		getGroups = customLookup.Getgroups
+	}
+
+	pwInfo, err := getPwUID(uint32(uid))
 	if err != nil || pwInfo == nil {
 		return content, err
 	}
 	if len(gids) == 0 {
-		grInfo, err := user.GetGrGID(pwInfo.GID)
+		grInfo, err := getGrGID(pwInfo.GID)
 		if err != nil || grInfo == nil {
 			return content, err
 		}
-		groups, err = os.Getgroups()
+		groups, err = getGroups()
 		if err != nil {
 			return content, err
 		}
@@ -77,7 +87,7 @@ func Group(path string, uid int, gids []int) (content []byte, err error) {
 	// only deduplicate newly added groups
 	deduplicateStrs := make(map[string]bool)
 	for _, gid := range groups {
-		grInfo, err := user.GetGrGID(uint32(gid))
+		grInfo, err := getGrGID(uint32(gid))
 		if err != nil || grInfo == nil {
 			sylog.Verbosef("Skipping GID %d as group entry doesn't exist.\n", gid)
 			continue

internal/pkg/util/fs/files/passwd.go
@@ -18,9 +18,15 @@ import (
 	"github.com/apptainer/apptainer/pkg/sylog"
 )
 
+type UserGroupLookup interface {
+	GetPwUID(uint32) (*user.User, error)
+	GetGrGID(uint32) (*user.Group, error)
+	Getgroups() ([]int, error)
+}
+
 // Passwd creates a passwd template based on content of file provided in path,
 // updates content with current user information and returns content.
-func Passwd(path string, home string, uid int) (content []byte, err error) {
+func Passwd(path string, home string, uid int, customLookup UserGroupLookup) (content []byte, err error) {
 	sylog.Verbosef("Checking for template passwd file: %s", path)
 	if !fs.IsFile(path) {
 		return content, fmt.Errorf("passwd file doesn't exist in container, not updating")
@@ -32,7 +38,12 @@ func Passwd(path string, home string, uid int) (content []byte, err error) {
 		return content, fmt.Errorf("failed to read passwd file content in container: %s", err)
 	}
 
-	pwInfo, err := user.GetPwUID(uint32(uid))
+	getPwUID := user.GetPwUID
+	if customLookup != nil {
+		getPwUID = customLookup.GetPwUID
+	}
+
+	pwInfo, err := getPwUID(uint32(uid))
 	if err != nil {
 		return content, err
 	}

pkg/runtime/engine/apptainer/config/config.go
@@ -69,6 +69,16 @@ type DMTCPConfig struct {
 	Args       []string `json:"args,omitempty"`
 }
 
+type UserInfo struct {
+	Username string         `json:"username,omitempty"`
+	Home     string         `json:"home,omitempty"`
+	UID      int            `json:"uid,omitempty"`
+	GID      int            `json:"gid,omitempty"`
+	Groups   map[int]string `json:"groups,omitempty"`
+	Gecos    string         `json:"gecos,omitempty"`
+	Shell    string         `json:"shell,omitempty"`
+}
+
 // JSONConfig stores engine specific configuration that is allowed to be set by the user.
 type JSONConfig struct {
 	ScratchDir            []string          `json:"scratchdir,omitempty"`
@@ -140,6 +150,7 @@ type JSONConfig struct {
 	DbusSessionBusAddress string            `json:"dbusSessionBusAddress,omitempty"`
 	NoEval                bool              `json:"noEval,omitempty"`
 	Underlay              bool              `json:"underlay,omitempty"`
+	UserInfo              UserInfo          `json:"userInfo,omitempty"`
 }
 
 // SetImage sets the container image path to be used by EngineConfig.JSON.

commit 62e8a70344debd306733e980bd08543ec6b642e3

Fix empty user's shell field in `/etc/passwd`
This regression was introduced by 4a0d2b81b1c2784d7df196575dfffb47478532a7

Signed-off-by: Dennis Klein <d.klein@gsi.de>

internal/pkg/runtime/engine/apptainer/prepare_linux.go
@@ -1516,17 +1516,14 @@ func (e *EngineOperations) setUserInfo(useTargetIDs bool) {
 
 	if useTargetIDs {
 		pw, err = user.GetPwUID(uint32(e.EngineConfig.GetTargetUID()))
-		if err == nil {
-			e.EngineConfig.JSON.UserInfo.Username = pw.Name
-			e.EngineConfig.JSON.UserInfo.Gecos = pw.Gecos
-			e.EngineConfig.JSON.UserInfo.UID = int(pw.UID)
-			e.EngineConfig.JSON.UserInfo.GID = int(pw.GID)
-		}
-	} else {
+	}
+
+	if err == nil {
 		e.EngineConfig.JSON.UserInfo.Username = pw.Name
 		e.EngineConfig.JSON.UserInfo.Gecos = pw.Gecos
 		e.EngineConfig.JSON.UserInfo.UID = int(pw.UID)
 		e.EngineConfig.JSON.UserInfo.GID = int(pw.GID)
+		e.EngineConfig.JSON.UserInfo.Shell = pw.Shell
 	}
 
 	e.EngineConfig.JSON.UserInfo.Groups = make(map[int]string)