sylabs/singularity

deps: replace oras-go with go-containerregistry
oras-go v1 has often caused issues due to it depending on old versions
of packages, and blocking dependency updates.

The current v2 of oras-go has a completely different structure, and
would require a comprehensive rewrite of our oras related code.

As we are using go-containerregistry for handling oci-sif push/pull,
we can use it for oras sif push/pull and drop oras-go. This also
requires a rewrite, but should ease the dependency situation going
forward.

Note that we now have to explicitly handle adding and removing items
from .singularity/docker-config.json in registry login/logout, as we
were using some oras-go v1 functions there before. go-cr will perform
unauthenticated GET against registries with a public `/v2/` endpoint,
so for the moment passwords are only verified on login when the
registry is fully private. Noted in changelog, may be addressed in
future.

Fixes #1262

e2e/pull/pull.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2019-2022, Sylabs Inc. All rights reserved.
+// Copyright (c) 2019-2023, Sylabs Inc. All rights reserved.
 // This software is licensed under a 3-clause BSD license. Please consult the
 // LICENSE.md file distributed with the sources of this project regarding your
 // rights to use or distribute this software.
@@ -9,7 +9,6 @@
 package pull
 
 import (
-	"context"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
@@ -18,16 +17,16 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/containerd/containerd/reference"
-	"github.com/containerd/containerd/remotes/docker"
+	"github.com/google/go-containerregistry/pkg/name"
+	"github.com/google/go-containerregistry/pkg/v1/remote"
+	"github.com/google/go-containerregistry/pkg/v1/types"
 	"github.com/sylabs/singularity/e2e/internal/e2e"
 	"github.com/sylabs/singularity/e2e/internal/testhelper"
+	"github.com/sylabs/singularity/internal/pkg/client/oras"
 	syoras "github.com/sylabs/singularity/internal/pkg/client/oras"
 	"github.com/sylabs/singularity/internal/pkg/util/uri"
 	"github.com/sylabs/singularity/pkg/image"
 	"golang.org/x/sys/unix"
-	"oras.land/oras-go/pkg/content"
-	"oras.land/oras-go/pkg/oras"
 )
 
 type ctx struct {
@@ -144,13 +143,7 @@ func (c *ctx) setup(t *testing.T) {
 	e2e.EnsureORASOCISIF(t, c.env)
 	e2e.EnsureRegistryOCISIF(t, c.env)
 
-	// setup file and dir to use as invalid images
-	orasInvalidDir, err := os.MkdirTemp(c.env.TestDir, "oras_push_dir-")
-	if err != nil {
-		t.Fatalf("unable to create src dir for push tests: %v", err)
-	}
-
-	orasInvalidFile, err := e2e.WriteTempFile(orasInvalidDir, "oras_invalid_image-", "Invalid Image Contents")
+	orasInvalidFile, err := e2e.WriteTempFile(c.env.TestDir, "oras_invalid_image-", "Invalid Image Contents")
 	if err != nil {
 		t.Fatalf("unable to create src file for push tests: %v", err)
 	}
@@ -172,11 +165,6 @@ func (c *ctx) setup(t *testing.T) {
 			srcPath:        c.env.ImagePath,
 			uri:            fmt.Sprintf("%s/pull_test_sif_mediatypeproto:latest", c.env.TestRegistry),
 			layerMediaType: syoras.SifLayerMediaTypeProto,
-		},
-		{
-			srcPath:        orasInvalidDir,
-			uri:            fmt.Sprintf("%s/pull_test_dir:latest", c.env.TestRegistry),
-			layerMediaType: syoras.SifLayerMediaTypeV1,
 		},
 		{
 			srcPath:        orasInvalidFile,
@@ -533,56 +521,24 @@ func checkPullResult(t *testing.T, tt testStruct) {
 // We can also set the layer mediaType - so we can push images with older media types
 // to verify that they can still be pulled.
 func orasPushNoCheck(path, ref, layerMediaType string) error {
+	ref = strings.TrimPrefix(ref, "oras://")
 	ref = strings.TrimPrefix(ref, "//")
 
-	spec, err := reference.Parse(ref)
+	// Get reference to image in the remote
+	ir, err := name.ParseReference(ref,
+		name.WithDefaultTag(name.DefaultTag),
+		name.WithDefaultRegistry(name.DefaultRegistry),
+	)
 	if err != nil {
-		return fmt.Errorf("unable to parse oci reference: %w", err)
+		return err
 	}
 
-	// Hostname() will panic if there is no '/' in the locator
-	// explicitly check for this and fail in order to prevent panic
-	// this case will only occur for incorrect uris
-	if !strings.Contains(spec.Locator, "/") {
-		return fmt.Errorf("not a valid oci object uri: %s", ref)
-	}
-
-	// append default tag if no object exists
-	if spec.Object == "" {
-		spec.Object = "latest"
-	}
-
-	resolver := docker.NewResolver(docker.ResolverOptions{})
-
-	store := content.NewFile("")
-	defer store.Close()
-
-	// Get the filename from path and use it as the name in the file store
-	name := filepath.Base(path)
-
-	desc, err := store.Add(name, layerMediaType, path)
+	im, err := oras.NewImageFromSIF(path, types.MediaType(layerMediaType))
 	if err != nil {
-		return fmt.Errorf("unable to add SIF to store: %w", err)
+		return err
 	}
 
-	manifest, manifestDesc, config, configDesc, err := content.GenerateManifestAndConfig(nil, nil, desc)
-	if err != nil {
-		return fmt.Errorf("unable to generate manifest and config: %w", err)
-	}
-
-	if err := store.Load(configDesc, config); err != nil {
-		return fmt.Errorf("unable to load config: %w", err)
-	}
-
-	if err := store.StoreManifest(spec.String(), manifestDesc, manifest); err != nil {
-		return fmt.Errorf("unable to store manifest: %w", err)
-	}
-
-	if _, err := oras.Copy(context.Background(), store, spec.String(), resolver, ""); err != nil {
-		return fmt.Errorf("unable to push: %w", err)
-	}
-
-	return nil
+	return remote.Write(ir, im, remote.WithUserAgent("singularity e2e-test"))
 }
 
 func (c ctx) testPullDisableCacheCmd(t *testing.T) {

internal/pkg/remote/credential/login_handler.go
@@ -1,3 +1,4 @@
+// Copyright (c) 2023, Sylabs Inc. All rights reserved.
 // Copyright (c) 2020, Control Command Inc. All rights reserved.
 // This software is licensed under a 3-clause BSD license. Please consult the
 // LICENSE.md file distributed with the sources of this project regarding your
@@ -8,15 +9,23 @@ package credential
 import (
 	"context"
 	"crypto/tls"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/url"
+	"os"
 	"time"
 
+	"github.com/docker/cli/cli/config"
+	"github.com/docker/cli/cli/config/configfile"
+	"github.com/docker/cli/cli/config/types"
+	"github.com/google/go-containerregistry/pkg/authn"
+	"github.com/google/go-containerregistry/pkg/name"
+	"github.com/google/go-containerregistry/pkg/v1/remote/transport"
+	"github.com/sylabs/singularity/internal/pkg/util/fs"
 	"github.com/sylabs/singularity/internal/pkg/util/interactive"
 	"github.com/sylabs/singularity/pkg/syfs"
 	useragent "github.com/sylabs/singularity/pkg/util/user-agent"
-	auth "oras.land/oras-go/pkg/auth/docker"
 )
 
 // loginHandlers contains the registered handlers by scheme.
@@ -61,6 +70,11 @@ func ensurePassword(password string) (string, error) {
 type ociHandler struct{}
 
 func (h *ociHandler) login(u *url.URL, username, password string, insecure bool) (*Config, error) {
+	if u == nil {
+		return nil, fmt.Errorf("URL not provided for login")
+	}
+	regName := u.Host + u.Path
+
 	if username == "" {
 		return nil, fmt.Errorf("Docker/OCI registry requires a username")
 	}
@@ -68,25 +82,106 @@ func (h *ociHandler) login(u *url.URL, username, password string, insecure bool)
 	if err != nil {
 		return nil, err
 	}
-	cli, err := auth.NewClient(syfs.DockerConf())
+
+	if err := checkOCILogin(regName, username, password, insecure); err != nil {
+		return nil, err
+	}
+
+	ociConfig := syfs.DockerConf()
+	ociConfigNew := syfs.DockerConf() + ".new"
+
+	cf := configfile.New(syfs.DockerConf())
+	if fs.IsFile(ociConfig) {
+		f, err := os.Open(ociConfig)
+		if err != nil {
+			return nil, err
+		}
+		defer f.Close()
+		cf, err = config.LoadFromReader(f)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	cf.AuthConfigs[regName] = types.AuthConfig{
+		Username: username,
+		Password: pass,
+	}
+
+	configData, err := json.Marshal(cf)
 	if err != nil {
 		return nil, err
 	}
-	if err := cli.Login(context.TODO(), u.Host+u.Path, username, pass, insecure); err != nil {
+	if err := os.WriteFile(ociConfigNew, configData, 0o600); err != nil {
 		return nil, err
 	}
+	if err := os.Rename(ociConfigNew, ociConfig); err != nil {
+		return nil, err
+	}
+
 	return &Config{
 		URI:      u.String(),
 		Insecure: insecure,
 	}, nil
 }
 
-func (h *ociHandler) logout(u *url.URL) error {
-	cli, err := auth.NewClient(syfs.DockerConf())
+func checkOCILogin(regName string, username, password string, insecure bool) error {
+	regOpts := []name.Option{}
+	if insecure {
+		regOpts = []name.Option{name.Insecure}
+	}
+	reg, err := name.NewRegistry(regName, regOpts...)
 	if err != nil {
 		return err
 	}
-	return cli.Logout(context.TODO(), u.Host+u.Path)
+
+	auth := authn.FromConfig(authn.AuthConfig{
+		Username: username,
+		Password: password,
+	})
+
+	scopes := []string{reg.Scope("")}
+
+	// Creating a new transport pings the registry and works through auth flow.
+	_, err = transport.NewWithContext(context.TODO(), reg, auth, http.DefaultTransport, scopes)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (h *ociHandler) logout(u *url.URL) error {
+	ociConfig := syfs.DockerConf()
+	ociConfigNew := syfs.DockerConf() + ".new"
+	cf := configfile.New(syfs.DockerConf())
+	if fs.IsFile(ociConfig) {
+		f, err := os.Open(ociConfig)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+		cf, err = config.LoadFromReader(f)
+		if err != nil {
+			return err
+		}
+	}
+
+	registry := u.Host + u.Path
+	if _, ok := cf.AuthConfigs[registry]; !ok {
+		return fmt.Errorf("%q is not logged in", registry)
+	}
+
+	delete(cf.AuthConfigs, registry)
+
+	configData, err := json.Marshal(cf)
+	if err != nil {
+		return err
+	}
+	if err := os.WriteFile(ociConfigNew, configData, 0o600); err != nil {
+		return err
+	}
+	return os.Rename(ociConfigNew, ociConfig)
 }
 
 // keyserverHandler handle login/logout for keyserver service.

apptainer/apptainer

e2e/pull/pull.go
@@ -13,7 +13,6 @@
 package pull
 
 import (
-	"context"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
@@ -24,13 +23,13 @@ import (
 
 	"github.com/apptainer/apptainer/e2e/internal/e2e"
 	"github.com/apptainer/apptainer/e2e/internal/testhelper"
+	"github.com/apptainer/apptainer/internal/pkg/client/oras"
 	syoras "github.com/apptainer/apptainer/internal/pkg/client/oras"
 	"github.com/apptainer/apptainer/internal/pkg/util/uri"
-	"github.com/containerd/containerd/reference"
-	"github.com/containerd/containerd/remotes/docker"
+	"github.com/google/go-containerregistry/pkg/name"
+	"github.com/google/go-containerregistry/pkg/v1/remote"
+	"github.com/google/go-containerregistry/pkg/v1/types"
 	"golang.org/x/sys/unix"
-	"oras.land/oras-go/pkg/content"
-	"oras.land/oras-go/pkg/oras"
 )
 
 type ctx struct {
@@ -140,13 +139,7 @@ func getImageNameFromURI(imgURI string) string {
 func (c *ctx) setup(t *testing.T) {
 	e2e.EnsureImage(t, c.env)
 
-	// setup file and dir to use as invalid images
-	orasInvalidDir, err := os.MkdirTemp(c.env.TestDir, "oras_push_dir-")
-	if err != nil {
-		t.Fatalf("unable to create src dir for push tests: %v", err)
-	}
-
-	orasInvalidFile, err := e2e.WriteTempFile(orasInvalidDir, "oras_invalid_image-", "Invalid Image Contents")
+	orasInvalidFile, err := e2e.WriteTempFile(c.env.TestDir, "oras_invalid_image-", "Invalid Image Contents")
 	if err != nil {
 		t.Fatalf("unable to create src file for push tests: %v", err)
 	}
@@ -168,11 +161,6 @@ func (c *ctx) setup(t *testing.T) {
 			srcPath:        c.env.ImagePath,
 			uri:            fmt.Sprintf("%s/pull_test_sif_mediatypeproto:latest", c.env.TestRegistry),
 			layerMediaType: syoras.SifLayerMediaTypeProto,
-		},
-		{
-			srcPath:        orasInvalidDir,
-			uri:            fmt.Sprintf("%s/pull_test_dir:latest", c.env.TestRegistry),
-			layerMediaType: syoras.SifLayerMediaTypeV1,
 		},
 		{
 			srcPath:        orasInvalidFile,
@@ -447,56 +435,24 @@ func checkPullResult(t *testing.T, tt testStruct) {
 // We can also set the layer mediaType - so we can push images with older media types
 // to verify that they can still be pulled.
 func orasPushNoCheck(path, ref, layerMediaType string) error {
+	ref = strings.TrimPrefix(ref, "oras://")
 	ref = strings.TrimPrefix(ref, "//")
 
-	spec, err := reference.Parse(ref)
+	// Get reference to image in the remote
+	ir, err := name.ParseReference(ref,
+		name.WithDefaultTag(name.DefaultTag),
+		name.WithDefaultRegistry(name.DefaultRegistry),
+	)
 	if err != nil {
-		return fmt.Errorf("unable to parse oci reference: %w", err)
+		return err
 	}
 
-	// Hostname() will panic if there is no '/' in the locator
-	// explicitly check for this and fail in order to prevent panic
-	// this case will only occur for incorrect uris
-	if !strings.Contains(spec.Locator, "/") {
-		return fmt.Errorf("not a valid oci object uri: %s", ref)
-	}
-
-	// append default tag if no object exists
-	if spec.Object == "" {
-		spec.Object = "latest"
-	}
-
-	resolver := docker.NewResolver(docker.ResolverOptions{})
-
-	store := content.NewFile("")
-	defer store.Close()
-
-	// Get the filename from path and use it as the name in the file store
-	name := filepath.Base(path)
-
-	desc, err := store.Add(name, layerMediaType, path)
+	im, err := oras.NewImageFromSIF(path, types.MediaType(layerMediaType))
 	if err != nil {
-		return fmt.Errorf("unable to add SIF to store: %w", err)
+		return err
 	}
 
-	manifest, manifestDesc, config, configDesc, err := content.GenerateManifestAndConfig(nil, nil, desc)
-	if err != nil {
-		return fmt.Errorf("unable to generate manifest and config: %w", err)
-	}
-
-	if err := store.Load(configDesc, config); err != nil {
-		return fmt.Errorf("unable to load config: %w", err)
-	}
-
-	if err := store.StoreManifest(spec.String(), manifestDesc, manifest); err != nil {
-		return fmt.Errorf("unable to store manifest: %w", err)
-	}
-
-	if _, err := oras.Copy(context.Background(), store, spec.String(), resolver, ""); err != nil {
-		return fmt.Errorf("unable to push: %w", err)
-	}
-
-	return nil
+	return remote.Write(ir, im, remote.WithUserAgent("singularity e2e-test"))
 }
 
 func (c ctx) testPullDisableCacheCmd(t *testing.T) {

internal/pkg/remote/credential/login_handler.go
@@ -12,18 +12,24 @@ package credential
 import (
 	"context"
 	"crypto/tls"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/url"
+	"os"
 	"time"
 
-	"oras.land/oras-go/pkg/auth"
-
+	"github.com/apptainer/apptainer/internal/pkg/util/fs"
 	"github.com/apptainer/apptainer/internal/pkg/util/interactive"
 	"github.com/apptainer/apptainer/pkg/syfs"
 
 	useragent "github.com/apptainer/apptainer/pkg/util/user-agent"
-	"oras.land/oras-go/pkg/auth/docker"
+	"github.com/docker/cli/cli/config"
+	"github.com/docker/cli/cli/config/configfile"
+	"github.com/docker/cli/cli/config/types"
+	"github.com/google/go-containerregistry/pkg/authn"
+	"github.com/google/go-containerregistry/pkg/name"
+	"github.com/google/go-containerregistry/pkg/v1/remote/transport"
 )
 
 // loginHandlers contains the registered handlers by scheme.
@@ -68,6 +74,11 @@ func ensurePassword(password string) (string, error) {
 type ociHandler struct{}
 
 func (h *ociHandler) login(u *url.URL, username, password string, insecure bool) (*Config, error) {
+	if u == nil {
+		return nil, fmt.Errorf("URL not provided for login")
+	}
+	regName := u.Host + u.Path
+
 	if username == "" {
 		return nil, fmt.Errorf("Docker/OCI registry requires a username")
 	}
@@ -75,44 +86,106 @@ func (h *ociHandler) login(u *url.URL, username, password string, insecure bool)
 	if err != nil {
 		return nil, err
 	}
-	cli, err := docker.NewClientWithDockerFallback(syfs.DockerConf())
-	if err != nil {
+
+	if err := checkOCILogin(regName, username, password, insecure); err != nil {
 		return nil, err
 	}
 
-	switch insecure {
-	case true:
-		if err := cli.LoginWithOpts(
-			auth.WithLoginContext(context.TODO()),
-			auth.WithLoginHostname(u.Host+u.Path),
-			auth.WithLoginUsername(username),
-			auth.WithLoginSecret(pass),
-			auth.WithLoginInsecure(),
-		); err != nil {
+	ociConfig := syfs.DockerConf()
+	ociConfigNew := syfs.DockerConf() + ".new"
+
+	cf := configfile.New(syfs.DockerConf())
+	if fs.IsFile(ociConfig) {
+		f, err := os.Open(ociConfig)
+		if err != nil {
 			return nil, err
 		}
-	case false:
-		if err := cli.LoginWithOpts(
-			auth.WithLoginContext(context.TODO()),
-			auth.WithLoginHostname(u.Host+u.Path),
-			auth.WithLoginUsername(username),
-			auth.WithLoginSecret(pass),
-		); err != nil {
+		defer f.Close()
+		cf, err = config.LoadFromReader(f)
+		if err != nil {
 			return nil, err
 		}
 	}
+
+	cf.AuthConfigs[regName] = types.AuthConfig{
+		Username: username,
+		Password: pass,
+	}
+
+	configData, err := json.Marshal(cf)
+	if err != nil {
+		return nil, err
+	}
+	if err := os.WriteFile(ociConfigNew, configData, 0o600); err != nil {
+		return nil, err
+	}
+	if err := os.Rename(ociConfigNew, ociConfig); err != nil {
+		return nil, err
+	}
+
 	return &Config{
 		URI:      u.String(),
 		Insecure: insecure,
 	}, nil
 }
 
-func (h *ociHandler) logout(u *url.URL) error {
-	cli, err := docker.NewClientWithDockerFallback(syfs.DockerConf())
+func checkOCILogin(regName string, username, password string, insecure bool) error {
+	regOpts := []name.Option{}
+	if insecure {
+		regOpts = []name.Option{name.Insecure}
+	}
+	reg, err := name.NewRegistry(regName, regOpts...)
 	if err != nil {
 		return err
 	}
-	return cli.Logout(context.TODO(), u.Host+u.Path)
+
+	auth := authn.FromConfig(authn.AuthConfig{
+		Username: username,
+		Password: password,
+	})
+
+	scopes := []string{reg.Scope("")}
+
+	// Creating a new transport pings the registry and works through auth flow.
+	_, err = transport.NewWithContext(context.TODO(), reg, auth, http.DefaultTransport, scopes)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (h *ociHandler) logout(u *url.URL) error {
+	ociConfig := syfs.DockerConf()
+	ociConfigNew := syfs.DockerConf() + ".new"
+	cf := configfile.New(syfs.DockerConf())
+	if fs.IsFile(ociConfig) {
+		f, err := os.Open(ociConfig)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+		cf, err = config.LoadFromReader(f)
+		if err != nil {
+			return err
+		}
+	}
+
+	registry := u.Host + u.Path
+	if _, ok := cf.AuthConfigs[registry]; !ok {
+		return fmt.Errorf("%q is not logged in", registry)
+	}
+
+	delete(cf.AuthConfigs, registry)
+
+	configData, err := json.Marshal(cf)
+	if err != nil {
+		return err
+	}
+	if err := os.WriteFile(ociConfigNew, configData, 0o600); err != nil {
+		return err
+	}
+	return os.Rename(ociConfigNew, ociConfig)
 }
 
 // keyserverHandler handle login/logout for keyserver service.