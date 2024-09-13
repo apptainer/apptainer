705b51bfea2f697a5ec35b56a21e31d358349ab4
Dave Dykstra <2129743+DrDaveD@users.noreply.github.com>
2024-09-13 17:13:46 -0500

    remove unnecessary nolint:revive
    Signed-off-by: Dave Dykstra <2129743+DrDaveD@users.noreply.github.com>

--- internal/app/apptainer/keyserver_list.go
+++ internal/app/apptainer/keyserver_list.go
@@ -23,8 +23,6 @@ import (
 
 // KeyserverList prints information about remote configurations
 // FIXME - remoteName is not being honored
-//
-//nolint:revive
 func KeyserverList(remoteName string, usrConfigFile string) (err error) {
 	c := &remote.Config{}
 
