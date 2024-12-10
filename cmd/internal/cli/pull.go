c4a80e54e315040d4b76f7297d6dac151b291dad
Xu YANG <2051711+JasonYangShadow@users.noreply.github.com>
2024-12-10 23:21:35 +0900

    search and update sylabs copyrights in recent cherry-picks (#2673)
    * update sylabs copyrights
    based on apptainer PR #2472
    
    Signed-off-by: jason yang <jasonyangshadow@gmail.com>
    
    * add sylabs copyright
    based on apptainer PR #2500
    
    Signed-off-by: jason yang <jasonyangshadow@gmail.com>
    
    * update sylabs copyrights
    based on apptainer PR #2535
    
    Signed-off-by: jason yang <jasonyangshadow@gmail.com>
    
    * update sylabs copyrights
    based on apptainer PR #2589
    
    Signed-off-by: jason yang <jasonyangshadow@gmail.com>
    
    * update sylabs copyrights
    based on apptainer PR #2590
    
    Signed-off-by: jason yang <jasonyangshadow@gmail.com>
    
    * update sylabs copyrights
    based on apptainer PR #2595
    
    Signed-off-by: jason yang <jasonyangshadow@gmail.com>
    
    * update sylabs copyrights
    based on apptainer PR #2654
    
    Signed-off-by: jason yang <jasonyangshadow@gmail.com>
    
    * update sylabs copyrights
    based on apptainer PR #2655
    
    Signed-off-by: jason yang <jasonyangshadow@gmail.com>
    
    ---------
    
    Signed-off-by: jason yang <jasonyangshadow@gmail.com>

--- cmd/internal/cli/pull.go
+++ cmd/internal/cli/pull.go
@@ -3,7 +3,7 @@
 //   For website terms of use, trademark policy, privacy policy and other
 //   project policies see https://lfprojects.org/policies
 // Copyright (c) 2020, Control Command Inc. All rights reserved.
-// Copyright (c) 2018-2021, Sylabs Inc. All rights reserved.
+// Copyright (c) 2018-2023, Sylabs Inc. All rights reserved.
 // This software is licensed under a 3-clause BSD license. Please consult the
 // LICENSE.md file distributed with the sources of this project regarding your
 // rights to use or distribute this software.
