6921a5a3044c43c5a8d3a47b9da585559007340a
Kirill Priadko <kirill.priadko@intel.com>
2025-03-26 16:45:10 +0200

    Add copyright headers
    Signed-off-by: Kirill Priadko <kirill.priadko@intel.com>

--- internal/pkg/util/gpu/hpu_test.go
+++ internal/pkg/util/gpu/hpu_test.go
@@ -1,3 +1,12 @@
+// Copyright (c) Contributors to the Apptainer project, established as
+//   Apptainer a Series of LF Projects LLC.
+//   For website terms of use, trademark policy, privacy policy and other
+//   project policies see https://lfprojects.org/policies
+// Copyright (c) 2025, Sylabs Inc. All rights reserved.
+// This software is licensed under a 3-clause BSD license. Please consult the
+// LICENSE.md file distributed with the sources of this project regarding your
+// rights to use or distribute this software.
+
 package gpu
 
 import (
