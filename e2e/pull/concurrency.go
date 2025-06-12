843ffefc801a6cdc3dd80502d690a17a869b191a
David Trudgian <david.trudgian@sylabs.io>
2025-06-12 16:16:28 +0200

    chore: update copyright headers on touched files
    Signed-off-by: Vaiva Zokaite <vaiva.zokaite@cern.ch>

--- e2e/pull/concurrency.go
+++ e2e/pull/concurrency.go
@@ -2,7 +2,7 @@
 //   Apptainer a Series of LF Projects LLC.
 //   For website terms of use, trademark policy, privacy policy and other
 //   project policies see https://lfprojects.org/policies
-// Copyright (c) 2021-2022, Sylabs Inc. All rights reserved.
+// Copyright (c) 2021-2025, Sylabs Inc. All rights reserved.
 // This software is licensed under a 3-clause BSD license. Please consult the
 // LICENSE.md file distributed with the sources of this project regarding your
 // rights to use or distribute this software.
@@ -159,6 +159,5 @@ func (c ctx) testConcurrentPulls(t *testing.T) {
 			// pull image
 			c.imagePull(t, ts)
 		})
-
 	}
 }
--- pkg/ocibundle/tools/loop.go
+++ pkg/ocibundle/tools/loop.go
@@ -2,7 +2,7 @@
 //   Apptainer a Series of LF Projects LLC.
 //   For website terms of use, trademark policy, privacy policy and other
 //   project policies see https://lfprojects.org/policies
-// Copyright (c) 2019-2023, Sylabs Inc. All rights reserved.
+// Copyright (c) 2019-2025, Sylabs Inc. All rights reserved.
 // This software is licensed under a 3-clause BSD license. Please consult the
 // LICENSE.md file distributed with the sources of this project regarding your
 // rights to use or distribute this software.
