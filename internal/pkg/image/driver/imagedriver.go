ea1138ed691eb57e418a5236c58ef2aecf2a3729
Dave Dykstra <2129743+DrDaveD@users.noreply.github.com>
2025-09-26 12:11:27 -0500

    update bundled fuse2fs and fuse-overlayfs
    Signed-off-by: Dave Dykstra <2129743+DrDaveD@users.noreply.github.com>

--- internal/pkg/image/driver/imagedriver.go
+++ internal/pkg/image/driver/imagedriver.go
@@ -639,6 +639,9 @@ func (i *fuseappsInstance) filterMsg() string {
 	ignoreMsgs := []string{
 		// from fuse2fs
 		"journal is not supported.",
+		"fuse2fs does not support using the journal.",
+		"There may be file system corruption",
+		"the file system is not gracefully unmounted.",
 		"Mounting read-only.",
 		// from squashfuse_ll
 		"failed to clone device fd",
