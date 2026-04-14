44ef302aea32c3e4bbd8e6c2b48db87cdc0ec6ad
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2026-04-14 21:40:19 +0200

    Consistent use of writable vs. writeable
    https://books.google.com/ngrams/graph?content=writable%2Cwriteable&year_start=1800&year_end=2022
    
    Signed-off-by: Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>

--- e2e/imgbuild/imgbuild.go
+++ e2e/imgbuild/imgbuild.go
@@ -1884,7 +1884,7 @@ func (c imgBuildTests) buildLibraryHost(t *testing.T) {
 	)
 }
 
-// testWritableTmpfs checks that we can run the build using a writeable tmpfs in the %test step
+// testWritableTmpfs checks that we can run the build using a writable tmpfs in the %test step
 func (c imgBuildTests) testWritableTmpfs(t *testing.T) {
 	e2e.EnsureImage(t, c.env)
 
--- internal/pkg/runtime/launch/options.go
+++ internal/pkg/runtime/launch/options.go
@@ -31,7 +31,7 @@ import (
 type launchOptions struct {
 	// Writable marks the container image itself as writable.
 	Writable bool
-	// WriteableTmpfs applies an ephemeral writable overlay to the container.
+	// WritableTmpfs applies an ephemeral writable overlay to the container.
 	WritableTmpfs bool
 	// OverlayPaths holds paths to image or directory overlays to be applied.
 	OverlayPaths []string
